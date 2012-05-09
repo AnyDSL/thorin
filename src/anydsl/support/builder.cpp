2c9aea2559dba2bb42ac189431eaaeda7833895a
Roland Leißa <leissa@cs.uni-saarland.de>
2012-05-09 09:20:43 +0200

    added arithop to builder

--- src/anydsl/support/builder.cpp
+++ src/anydsl/support/builder.cpp
@@ -3,6 +3,15 @@
 namespace anydsl {
 
 
+ArithOp* Builder::createArithOp(ArithOpKind arithOpKind,
+                                Def* ldef, Def* rdef, 
+                                const std::string& ldebug /*= ""*/, 
+                                const std::string& rdebug /*= ""*/, 
+                                const std::string&  debug /*= ""*/) {
+    // TODO
+
+    return new ArithOp(arithOpKind, ldef, rdef, ldebug, rdebug, debug);
+}
 
 
 
--- src/anydsl/support/builder.h
+++ src/anydsl/support/builder.h
@@ -1,20 +1,32 @@
 #ifndef ANYDSL_SUPPORT_BUILDER_H
 #define ANYDSL_SUPPORT_BUILDER_H
 
+#include "anydsl/air/primop.h"
+
 namespace anydsl {
+
+class Universe;
 
 class ArithOp;
 
 class Builder {
 public:
 
-    Builder();
+    Builder(Universe& universe);
     ~Builder();
 
 
-    //ArithOp* createArithOp(
+    ArithOp* createArithOp(ArithOpKind arithOpKind,
+                           Def* ldef, Def* rdef, 
+                           const std::string& ldebug = "", 
+                           const std::string& rdebug = "", 
+                           const std::string&  debug = "");
+
+    const Universe& universe() const { return universe_; } 
 
 private:
+
+    Universe& universe_;
 };
 
 } // namespace anydsl

b68eeb3016459b4a256390e9a8dfa96052129905
Roland Leißa <leissa@cs.uni-saarland.de>
2012-05-11 09:23:05 +0200

    reworked enum magic

--- src/anydsl/air/enums.h
+++ src/anydsl/air/enums.h
@@ -5,80 +5,68 @@ namespace anydsl {
 
 //------------------------------------------------------------------------------
 
-namespace detail {
-
-enum CountPrimTypes_u {
-#define ANYDSL_U_TYPE(T) CountType_u_##T,
-#include "anydsl/tables/primtypetable.h"
-    Num_PrimTypes_u,
-};
-
-enum CountPrimTypes_f {
-#define ANYDSL_F_TYPE(T) CountType_f_##T,
-#include "anydsl/tables/primtypetable.h"
-    Num_PrimTypes_f,
-};
-
-enum CountNodes {
-#define ANYDSL_AIR_NODE(node) CountNode_##node,
-#include "anydsl/tables/nodetable.h"
-    Num_Nodes,
-};
-
-enum CountArithOps {
-#define ANYDSL_ARITHOP(op) CountArithOp_##op,
-#include "anydsl/tables/arithoptable.h"
-    Num_ArithOps,
-};
-
-enum CountRelOps {
-#define ANYDSL_RELOP(op) CountRelOp_##op,
-#include "anydsl/tables/reloptable.h"
-    Num_RelOps,
-};
-
-enum CountConvOps {
-#define ANYDSL_CONVOP(op) CountConvOp_##op,
-#include "anydsl/tables/convoptable.h"
-    Num_ConvOps,
-};
-
-} // namespace detail
-
-//------------------------------------------------------------------------------
-
-enum Counter {
-    Num_PrimTypes_u = detail::Num_PrimTypes_u,
-    Num_PrimTypes_f = detail::Num_PrimTypes_f,
-    Num_PrimTypes   = detail::Num_PrimTypes_u + detail::Num_PrimTypes_f,
-
-    Num_Nodes = detail::Num_Nodes,
-
-    Num_ArithOps = detail::Num_ArithOps,
-    Num_RelOps   = detail::Num_RelOps,
-    Num_ConvOps  = detail::Num_ConvOps,
-
-    Num_Indexes = Num_Nodes + Num_ArithOps 
-                            + Num_RelOps 
-                            + Num_ConvOps
-};
+#define ANYDSL_GLUE(pre, next) \
+    End_##pre, \
+    Begin_##next = End_##pre, \
+    __##Begin_##next = Begin_##next - 1,
 
 enum IndexKind {
+    Begin_Node = 0,
+    __Begin_Node = -1,
 #define ANYDSL_AIR_NODE(node) Index_##node,
 #include "anydsl/tables/nodetable.h"
 
-#define ANYDSL_U_TYPE(T) Index_##T,
-#define ANYDSL_F_TYPE(T) Index_##T,
+    ANYDSL_GLUE(Node, PrimType_u)
+
+#define ANYDSL_U_TYPE(T) Index_PrimType_##T,
 #include "anydsl/tables/primtypetable.h"
+
+    ANYDSL_GLUE(PrimType_u, PrimType_f)
+
+#define ANYDSL_F_TYPE(T) Index_PrimType_##T,
+#include "anydsl/tables/primtypetable.h"
+
+    ANYDSL_GLUE(PrimType_f, PrimConst_u)
+
+#define ANYDSL_U_TYPE(T) Index_PrimConst_##T,
+#include "anydsl/tables/primtypetable.h"
+
+    ANYDSL_GLUE(PrimConst_u, PrimConst_f)
+
+#define ANYDSL_F_TYPE(T) Index_PrimConst_##T,
+#include "anydsl/tables/primtypetable.h"
+
+    ANYDSL_GLUE(PrimConst_f, ArithOp)
 
 #define ANYDSL_ARITHOP(op) Index_##op,
 #include "anydsl/tables/arithoptable.h"
 
+    ANYDSL_GLUE(ArithOp, RelOp)
+
 #define ANYDSL_RELOP(op) Index_##op,
 #include "anydsl/tables/reloptable.h"
 
+    ANYDSL_GLUE(RelOp, ConvOp)
+
 #define ANYDSL_CONVOP(op) Index_##op,
 #include "anydsl/tables/convoptable.h"
+    End_ConvOp,
+
+    Num_Indexes = End_ConvOp,
+
+    Num_Nodes        = End_Node        - Begin_Node,
+
+    Num_PrimTypes_u  = End_PrimType_u  - Begin_PrimType_u,
+    Num_PrimTypes_f  = End_PrimType_f  - Begin_PrimType_f,
+    Num_PrimConsts_u = End_PrimConst_u - Begin_PrimConst_u,
+    Num_PrimConsts_f = End_PrimConst_f - Begin_PrimConst_f,
+
+    Num_ArithOps     = End_ArithOp     - Begin_ArithOp,
+    Num_RelOps       = End_RelOp       - Begin_RelOp,
+    Num_ConvOps      = End_ConvOp      - Begin_ConvOp,
+
+    Num_PrimTypes = Num_PrimTypes_u + Num_PrimTypes_f,
+    Num_PrimConsts = Num_PrimTypes,
 };
 
 enum NodeKind {
@@ -87,14 +75,14 @@ enum NodeKind {
 };
 
 enum PrimTypeKind {
-#define ANYDSL_U_TYPE(T) PrimType_##T = Index_##T,
-#define ANYDSL_F_TYPE(T) PrimType_##T = Index_##T,
+#define ANYDSL_U_TYPE(T) PrimType_##T = Index_PrimType_##T,
+#define ANYDSL_F_TYPE(T) PrimType_##T = Index_PrimType_##T,
 #include "anydsl/tables/primtypetable.h"
 };
 
-enum PrimKind {
-#define ANYDSL_U_TYPE(T) Prim_##T = Index_##T,
-#define ANYDSL_F_TYPE(T) Prim_##T = Index_##T,
+enum PrimConstKind {
+#define ANYDSL_U_TYPE(T) Prim_##T = Index_PrimConst_##T,
+#define ANYDSL_F_TYPE(T) Prim_##T = Index_PrimConst_##T,
 #include "anydsl/tables/primtypetable.h"
 };
 

f5838ea0edfede68e2114c78204403527126f5df
Roland Leißa <leissa@cs.uni-saarland.de>
2012-05-26 15:00:07 +0200

    error guardes in allindices

--- src/anydsl/tables/allindices.h
+++ src/anydsl/tables/allindices.h
@@ -1,3 +1,31 @@
+#ifndef ANYDSL_GLUE
+#error "define ANYDSL_GLUE before including this file"
+#endif
+
+#ifndef ANYDSL_AIR_NODE
+#error "define ANYDSL_AIR_NODE before including this file"
+#endif
+
+#ifndef ANYDSL_PRIMTYPE
+#error "define ANYDSL_PRIMTYPE before including this file"
+#endif
+
+#ifndef ANYDSL_PRIMLIT
+#error "define ANYDSL_PRIMLIT before including this file"
+#endif
+
+#ifndef ANYDSL_ARITHOP
+#error "define ANYDSL_ARITHOP before including this file"
+#endif
+
+#ifndef ANYDSL_RELOP
+#error "define ANYDSL_RELOP before including this file"
+#endif
+
+#ifndef ANYDSL_CONVOP
+#error "define ANYDSL_CONVOP before including this file"
+#endif
+
 #include "anydsl/tables/nodetable.h"
     ANYDSL_GLUE(Node, PrimType_u)
 #define ANYDSL_U_TYPE(T) ANYDSL_PRIMTYPE(T)

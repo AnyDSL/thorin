9f4b144847adcebd3ae5bec88999ae73c654f201
Roland Leißa <leissa@cs.uni-saarland.de>
2012-07-27 16:57:35 +0200

    removed useless members

--- src/anydsl/def.cpp
+++ src/anydsl/def.cpp
@@ -37,12 +37,6 @@ void Def::unregisterUse(size_t i, const Def* def) const {
     uses_.erase(use);
 }
 
-bool Def::isType() const { 
-    // don't use enum magic here -- there may be user defined types
-    anydsl_assert(!type_ ^ bool(isa<Type>()), "inconsistent");
-    return !type_;
-}
-
 World& Def::world() const { 
     if (type_)
         return type_->world(); 
--- src/anydsl/def.h
+++ src/anydsl/def.h
@@ -118,11 +118,6 @@ public:
 
     int kind() const { return kind_; }
     bool isCoreNode() const { return ::anydsl::isCoreNode(kind()); }
-    bool isPrimType() const { return ::anydsl::isPrimType(kind()); }
-    bool isArithOp()  const { return ::anydsl::isArithOp(kind()); }
-    bool isRelOp()    const { return ::anydsl::isRelOp(kind()); }
-    bool isConvOp()   const { return ::anydsl::isConvOp(kind()); }
-    bool isType() const;
 
     IndexKind indexKind() const { assert(isCoreNode()); return (IndexKind) kind_; }
 

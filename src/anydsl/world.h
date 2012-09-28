407aa75bbb544280bfcc0309a9ac86e8310278bd
Roland Leißa <leissa@cs.uni-saarland.de>
2012-09-28 13:43:42 +0200

    removed comment

--- src/anydsl/world.h
+++ src/anydsl/world.h
@@ -29,8 +29,6 @@ class Sigma;
 class Slot;
 class Type;
 
-// TODO port this stuff to boost::intrusive 
-// -> we can get then rid of all the new-calls!!!
 typedef boost::unordered_set<const PrimOp*, NodeHash, NodeEqual> PrimOpSet;
 typedef boost::unordered_set<Lambda*> LambdaSet;
 typedef boost::unordered_set<const Type*, NodeHash, NodeEqual> TypeSet;

b19717b98057d16a35214cd96ec3da7b970ba4d5
Roland Leißa <leissa@cs.uni-saarland.de>
2013-02-21 16:45:19 +0100

    clean up

--- src/anydsl2/transform/merge_lambdas.cpp
+++ src/anydsl2/transform/merge_lambdas.cpp
@@ -8,32 +8,17 @@
 
 namespace anydsl2 {
 
-class Merger {
-public:
-
-    Merger(World& world)
-        : world(world)
-    {}
-
-    void merge();
-
-private:
-
-    World& world;
-};
-
-void Merger::merge() {
+void merge_lambdas(World& world) {
     std::vector<Lambda*> todo;
     for_all (top, find_root_lambdas(world)) {
         Scope scope(top);
-        const DomTree& dt = scope.domtree();
         for (size_t i = scope.size(); i-- != 0;) {
             Lambda* lambda = scope.rpo(i);
             if (lambda->num_uses() == 1 && !lambda->attr().is_extern()) {
                 Use use = *lambda->uses().begin();
                 if (use.index() == 0) {
                     Lambda* ulambda = use.def()->as_lambda();
-                    if (scope.contains(ulambda) && dt.dominates(ulambda, lambda))
+                    if (scope.contains(ulambda) && scope.domtree().dominates(ulambda, lambda))
                         todo.push_back(lambda);
                 }
             }
@@ -48,20 +33,6 @@ void Merger::merge() {
         dropped->destroy_body();
         lambda->destroy_body();
     }
-#if 0
-    for_all (lambda, world.lambdas()) {
-        if (lambda->num_uses() == 1 && !lambda->attr().is_extern() && lambda->num_params() == 0) {
-            Use use = *lambda->uses().begin();
-            if (use.index() == 0) {
-                Lambda* ulambda = use.def()->as_lambda();
-                ulambda->jump(lambda->to(), lambda->args());
-                lambda->destroy_body();
-            }
-        }
-    }
-#endif
 }
-
-void merge_lambdas(World& world) { Merger(world).merge(); }
 
 } // namespace anydsl2

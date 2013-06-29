1881634e645e74334c9f0784d0c4a068d1323fc8
Roland Leißa <leissa@cs.uni-saarland.de>
2013-06-29 12:43:38 +0200

    simplified code

--- src/anydsl2/transform/mem2reg.cpp
+++ src/anydsl2/transform/mem2reg.cpp
@@ -8,25 +8,6 @@
 
 namespace anydsl2 {
 
-struct Replace {
-    Replace(const Load* load, const Def* with)
-        : access_(load)
-        , with_(with)
-    {}
-    Replace(const Store* store)
-        : access_(store)
-        , with_(0)
-    {}
-
-    const Access* access_;
-    const Def* with_;
-};
-
-void init_lambda(const size_t pass, Lambda* lambda) {
-    if (!lambda->visit(pass))
-        lambda->counter = lambda->preds().size();
-}
-
 void mem2reg(World& world) {
     // mark lambdas passed to other functions as head
     // -> Lambda::get_value will stop at function heads
@@ -41,7 +22,7 @@ void mem2reg(World& world) {
     for_all (root, top_level_lambdas(world)) {
         Scope scope(root);
         std::vector<const Def*> visit = visit_late(scope);
-        std::vector<Replace> to_replace;
+        std::vector<const Access*> accesses;
         const size_t pass = world.new_pass();
         size_t cur_handle = 0;
 
@@ -49,8 +30,10 @@ void mem2reg(World& world) {
         for_all (def, visit) {
             if (Lambda* lambda = def->isa_lambda()) {
                 if (cur) {
+                    // seal successors of last lambda if applicable
                     for_all (succ, cur->succs()) {
-                        init_lambda(pass, succ);
+                        if (!succ->visit(pass))
+                            succ->counter = succ->preds().size();
                         if (--succ->counter == 0)
                             succ->seal();
                     }
@@ -69,14 +52,15 @@ void mem2reg(World& world) {
                 if (const Slot* slot = store->ptr()->isa<Slot>()) {
                     if (slot->counter != size_t(-1)) { // if not address taken
                         cur->set_value(slot->counter, store->val());
-                        to_replace.push_back(Replace(store));
+                        accesses.push_back(store);
                     }
                 }
             } else if (const Load* load = def->isa<Load>()) {
                 if (const Slot* slot = load->ptr()->isa<Slot>()) {
                     if (slot->counter != size_t(-1)) { // if not address taken
                         const Type* type = slot->type()->as<Ptr>()->referenced_type();
-                        to_replace.push_back(Replace(load, cur->get_value(slot->counter, type, slot->name.c_str())));
+                        load->cptr = cur->get_value(slot->counter, type, slot->name.c_str());
+                        accesses.push_back(load);
                     }
                 }
             } else if (const Enter* enter = def->isa<Enter>()) {
@@ -86,21 +70,19 @@ void mem2reg(World& world) {
             }
         }
 
+        // seal successors of last block
         for_all (succ, cur->succs()) {
             assert(succ->is_visited(pass));
             assert(succ->counter == 1);
             succ->seal();
         }
 
-        for (size_t i = to_replace.size(); i-- != 0;) {
-            Replace replace = to_replace[i];
-
-            if (const Def* with = replace.with_) {
-                const Load* load = replace.access_->as<Load>();
-                load->extract_val()->replace(with);
+        for (size_t i = accesses.size(); i-- != 0;) {
+            if (const Load* load = accesses[i]->isa<Load>()) {
+                load->extract_val()->replace((const Def*) load->cptr);
                 load->extract_mem()->replace(load->mem());
             } else {
-                const Store* store = replace.access_->as<Store>();
+                const Store* store = accesses[i]->as<Store>();
                 store->replace(store->mem());
             }
         }

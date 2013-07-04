3969f09150d72744e30fc75a10ee6a170c96fc85
Roland Leißa <leissa@cs.uni-saarland.de>
2013-07-05 01:37:15 +0200

    added comments

--- src/anydsl2/transform/mem2reg.cpp
+++ src/anydsl2/transform/mem2reg.cpp
@@ -36,35 +36,38 @@ void mem2reg(World& world) {
         for (size_t i = 0, e = scope.size(); i != e; ++i) {
             Lambda* lambda = scope[i];
 
+            // Search for slots/loads/stores from top down and use set_value/get_value to install parameters.
+            // Then, we now what must be replaced but do not yet replace anything:
+            // Defs in the schedule might get invalid!
             for_all (primop, schedule[i]) {
                 if (const Slot* slot = primop->isa<Slot>()) {
                     // are all users loads and store?
                     for_all (use, slot->uses()) {
                         if (!use->isa<Load>() && !use->isa<Store>()) {
-                            slot->counter = size_t(-1); // mark as "address taken"
+                            slot->counter = size_t(-1);     // mark as "address taken"
                             goto next_primop;
                         }
                     }
                     slot->counter = cur_handle++;
                 } else if (const Store* store = primop->isa<Store>()) {
                     if (const Slot* slot = store->ptr()->isa<Slot>()) {
-                        if (slot->counter != size_t(-1)) { // if not "address taken"
+                        if (slot->counter != size_t(-1)) {  // if not "address taken"
                             lambda->set_value(slot->counter, store->val());
                             accesses.push_back(store);
                         }
                     }
                 } else if (const Load* load = primop->isa<Load>()) {
                     if (const Slot* slot = load->ptr()->isa<Slot>()) {
-                        if (slot->counter != size_t(-1)) { // if not "address taken"
+                        if (slot->counter != size_t(-1)) {  // if not "address taken"
                             const Type* type = slot->type()->as<Ptr>()->referenced_type();
                             load->cptr = lambda->get_value(slot->counter, type, slot->name.c_str());
                             accesses.push_back(load);
                         }
                     }
                 } else if (const Enter* enter = primop->isa<Enter>()) {
-                    enters.push_back(new Tracker(enter));
+                    enters.push_back(new Tracker(enter));   // keep track of Enters - they might get superfluous 
                 } else if (const Leave* leave = primop->isa<Leave>()) {
-                    leaves.push_back(new Tracker(leave));
+                    leaves.push_back(new Tracker(leave));   // keep track of Leaves - they might get superfluous 
                 }
             }
 
@@ -79,6 +82,7 @@ void mem2reg(World& world) {
             }
         }
 
+        // now replace everything from bottom up
         for (size_t i = accesses.size(); i-- != 0;) {
             if (const Load* load = accesses[i]->isa<Load>()) {
                 load->extract_val()->replace((const Def*) load->cptr);
@@ -95,8 +99,10 @@ next_primop:;
     for_all (lambda, world.lambdas())
         lambda->clear();
 
+    // this will wipe out dead Slots
     world.cleanup();
 
+    // are there any Leave/Enter pairs superfluous?
     for (size_t i = leaves.size(); i-- != 0;) {
         const Leave* leave = leaves[i]->def()->as<Leave>();
         const Enter* enter = leave->frame()->as<TupleExtract>()->tuple()->as<Enter>();
@@ -112,6 +118,7 @@ next_primop:;
 next_leave:;
     }
 
+    // are there superfluous poor, lonely Enters? no mercy - eliminate them
     for (size_t i = enters.size(); i-- != 0;) {
         if (const Enter* enter = enters[i]->def()->isa<Enter>()) {
             if (enter->extract_frame()->num_uses() == 0)

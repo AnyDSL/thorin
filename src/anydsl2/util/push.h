823231f53efa3b2f7305845f459fa6bbc34055fb
Roland Leißa <leissa@cs.uni-saarland.de>
2013-08-09 02:24:57 +0200

    made PUSH magic more convenient

--- src/anydsl2/util/push.h
+++ src/anydsl2/util/push.h
@@ -1,6 +1,10 @@
 #ifndef ANYDSL2_UTIL_PUSH_H
 #define ANYDSL2_UTIL_PUSH_H
 
+#define ANYDSL2_LNAME__(name, line) name##__##line
+#define ANYDSL2_LNAME_(name, line)  ANYDSL2_LNAME__(name, line)
+#define ANYDSL2_LNAME(name)         ANYDSL2_LNAME_(name, __LINE__)
+ 
 namespace anydsl2 {
 
 template<class T>
@@ -12,10 +16,16 @@ struct Push {
         t = new_val; 
     }
     ~Push() { ref_ = old_; }
+
 private:
     T old_;
     T& ref_;
 };
+
+template<class T, class U>
+inline Push<T> push(T& t, U new_val) { return Push<T>(t, new_val); }
+
+#define ANYDSL2_PUSH(what, with) auto ANYDSL2_LNAME(anydsl2_push) = anydsl2::push((what), (with))
 
 } // namespace anydsl2
 

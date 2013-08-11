fbd42d4d791c93135494ab21388ef4ad87ad91eb
Roland Leißa <leissa@cs.uni-saarland.de>
2013-08-12 01:02:32 +0200

    u1 now part in Box union (due to C++11 now possible)

--- src/anydsl2/util/box.h
+++ src/anydsl2/util/box.h
@@ -14,27 +14,22 @@ union Box {
 public:
 
     Box()      { reset(); }
-    Box( u1 u) { reset(); bool_ = u.get(); }
+    Box( u1 u) { reset();   u1_ = u; }
     Box( u8 u) { reset();   u8_ = u; }
     Box(u16 u) { reset();  u16_ = u; }
     Box(u32 u) { reset();  u32_ = u; }
     Box(u64 u) { reset();  u64_ = u; }
     Box(f32 f) { reset();  f32_ = f; }
     Box(f64 f) { reset();  f64_ = f; }
-
-    Box(bool b)  { reset(); bool_ = b; }
+    Box(bool b)  { reset();   u1_ = b; }
     Box(char c)  { reset(); char_ = c; }
     Box(void* p) { reset(); ptr_  = p; }
     Box(const_char_ptr s) { reset(); const_char_ptr_ = s; }
 
-    bool operator == (const Box& other) const {
-        return bcast<uint64_t, Box>(*this) == bcast<uint64_t, Box>(other);
-    }
-
-    template <typename T>
-    inline T get() { ANYDSL2_UNREACHABLE; }
-
-     u1  get_u1() const { return u1(bool_); }
+    bool operator == (const Box& other) const { return bcast<uint64_t, Box>(*this) == bcast<uint64_t, Box>(other); }
+    // see blow for specializations
+    template <typename T> inline T get() { ANYDSL2_UNREACHABLE; } 
+     u1  get_u1() const { return u1_; }
      u8  get_u8() const { return u8_; }
     u16 get_u16() const { return u16_; }
     u32 get_u32() const { return u32_; }
@@ -47,28 +42,25 @@ private:
 
     void reset() { memset(this, 0, sizeof(Box)); }
 
+    u1    u1_;
     u8    u8_;
     u16  u16_;
     u32  u32_;
     u64  u64_;
     f32  f32_;
     f64  f64_;
-
-    bool bool_;
     char  char_;
     void* ptr_;
     const_char_ptr const_char_ptr_;
-
 };
 
-template <> inline  u1 Box::get< u1>() { return  u1(bool_); }
+template <> inline  u1 Box::get< u1>() { return  u1_; }
 template <> inline  u8 Box::get< u8>() { return  u8_; }
 template <> inline u16 Box::get<u16>() { return u16_; }
 template <> inline u32 Box::get<u32>() { return u32_; }
 template <> inline u64 Box::get<u64>() { return u64_; }
 template <> inline f32 Box::get<f32>() { return f32_; }
 template <> inline f64 Box::get<f64>() { return f64_; }
-template <> inline bool Box::get<bool>() { return bool_; }
 template <> inline const_char_ptr Box::get<const_char_ptr>() { return const_char_ptr_; }
 template <> inline char Box::get<char>() { return char_; }
 template <> inline void* Box::get<void*>() { return ptr_; }

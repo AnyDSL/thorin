5501d4ca7505bd8528737fe5bf06bf28502378b6
Roland Leißa <leissa@cs.uni-saarland.de>
2013-09-23 13:40:40 +0200

    fixed warning

--- src/anydsl2/world.cpp
+++ src/anydsl2/world.cpp
@@ -75,7 +75,7 @@ World::~World() {
 Sigma* World::named_sigma(size_t size, const std::string& name) {
     Sigma* s = new Sigma(*this, size, name);
     assert(types_.find(s) == types_.end() && "must not be inside");
-    types_.insert(s).first;
+    types_.insert(s);
     return s;
 }
 

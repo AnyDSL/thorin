5e0749e3e6e49d1f1bc71fbb056e8ceb0dfe49fe
Roland Leißa <leissa@cs.uni-saarland.de>
2013-10-30 00:46:47 +0100

    modularizing mem2reg

--- src/anydsl2/transform/mem2reg.h
+++ src/anydsl2/transform/mem2reg.h
@@ -3,12 +3,16 @@
 
 namespace anydsl2 {
 
+class Scope;
+class World;
+
 /**
  * Tries to remove \p Load%s, \p Store%s, and \p Slot%s in favor of \p Param%s and arguments of function calls.
  * Moreover, superflous \p Enter and \p Leave \p PrimOp%s are removed.
  * \attention { Currently, this transformation only works when in CFF. }
  */
 void mem2reg(World&);
+void mem2reg(const Scope&);
 
 } // namespace anydsl2
 

6011be7af66d1b3e2bf49a3720f0167e7d2baf8a
Roland Leißa <leissa@cs.uni-saarland.de>
2013-12-16 01:42:19 +0100

    updated tables

--- src/thorin/tables/convoptable.h
+++ src/thorin/tables/convoptable.h
@@ -2,15 +2,7 @@
 #error "define THORIN_CONVOP before including this file"
 #endif
 
-THORIN_CONVOP(trunc)
-THORIN_CONVOP(zext)
-THORIN_CONVOP(sext)
-THORIN_CONVOP(stof)
-THORIN_CONVOP(utof)
-THORIN_CONVOP(ftrunc)
-THORIN_CONVOP(fext)
-THORIN_CONVOP(ftos)
-THORIN_CONVOP(ftou)
+THORIN_CONVOP(convert)
 THORIN_CONVOP(bitcast)
 THORIN_CONVOP(inttoptr)
 THORIN_CONVOP(ptrtoint)

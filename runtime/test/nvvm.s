a0462cbb2f1fe14ba00761b5f647ef4df3d5117f
Marcel Köster <koester@cs.uni-saarland.de>
2013-12-18 00:45:25 +0100

    Added NVVM runtime definitions and adapted simple example.

---
+++ runtime/test/nvvm.s
@@ -0,0 +1,14 @@
+; Module nvvm-thorin runtime decls
+
+declare i64 @nvvm_malloc_memory(i64);
+declare i64 @nvvm_free_memory(i64);
+
+declare void @nvvm_write_memory(i64, i8*, i64);
+declare void @nvvm_read_memory(i64, i8*, i64);
+
+declare void @nvvm_set_problem_size(i64, i64, i64);
+declare void @nvvm_synchronize();
+
+declare void @nvvm_set_kernel_arg(i64*);
+declare void @nvvm_load_kernel(i8*, i8*);
+declare void @nvvm_launch_kernel(i8*);

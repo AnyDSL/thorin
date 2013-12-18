6a594caee0c60c8b81e5e20038552669ee031ece
Roland Leißa <leissa@cs.uni-saarland.de>
2013-12-18 17:55:48 +0100

    trying to guess target datalyout

--- src/thorin/be/llvm/cpu.cpp
+++ src/thorin/be/llvm/cpu.cpp
@@ -1,5 +1,6 @@
 #include "thorin/be/llvm/cpu.h"
 
+#include <llvm/ADT/Triple.h>
 #include <llvm/Support/Host.h>
 #include <llvm/ExecutionEngine/ExecutionEngine.h>
 
@@ -9,7 +10,11 @@ namespace thorin {
 CPUCodeGen::CPUCodeGen(World& world)
     : CodeGen(world, llvm::CallingConv::C)
 {
-    //llvm::ExecutionEngine engine(module_);
+    module_->setTargetTriple(llvm::sys::getDefaultTargetTriple());
+    //llvm::EngineBuilder builder(module_);
+    //auto machine = builder.selectTarget();
+    //assert(machine);
+    //module_->setDataLayout(machine->getDataLayout()->getStringRepresentation());
 }
 
 }

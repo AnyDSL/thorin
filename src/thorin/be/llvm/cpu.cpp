a99dc1d67304fc461f464f149176adaf5f7cdf0d
Roland Leißa <leissa@cs.uni-saarland.de>
2013-12-18 17:31:07 +0100

    removed '<' '>' from fun expr

--- src/thorin/be/llvm/cpu.cpp
+++ src/thorin/be/llvm/cpu.cpp
@@ -1,13 +1,15 @@
 #include "thorin/be/llvm/cpu.h"
 
 #include <llvm/Support/Host.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+
 
 namespace thorin {
 
 CPUCodeGen::CPUCodeGen(World& world)
     : CodeGen(world, llvm::CallingConv::C)
 {
-    module_->setTargetTriple(llvm::sys::getDefaultTargetTriple());
+    //llvm::ExecutionEngine engine(module_);
 }
 
 }

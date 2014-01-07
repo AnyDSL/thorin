d62e4cdfef934ac6ace2698e9591c96603db5e5a
Roland Leißa <leissa@cs.uni-saarland.de>
2014-01-07 22:32:39 +0100

    bug fix for Box

--- src/thorin/util/box.h
+++ src/thorin/util/box.h
@@ -13,6 +13,16 @@ public:
     Box()      { reset(); }
 #define THORIN_ALL_TYPE(T) Box(T val) { reset(); T##_ = val; }
 #include "thorin/tables/primtypetable.h"
+    Box(  int8_t val) { reset();   int8_t_ = val; }
+    Box( int16_t val) { reset();  int16_t_ = val; }
+    Box( int32_t val) { reset();  int32_t_ = val; }
+    Box( int64_t val) { reset();  int64_t_ = val; }
+    Box( uint8_t val) { reset();  uint8_t_ = val; }
+    Box(uint16_t val) { reset(); uint16_t_ = val; }
+    Box(uint32_t val) { reset(); uint32_t_ = val; }
+    Box(uint64_t val) { reset(); uint64_t_ = val; }
+    Box(float    val) { reset(); float_    = val; }
+    Box(double   val) { reset(); double_   = val; }
 
     bool operator == (const Box& other) const { return bcast<uint64_t, Box>(*this) == bcast<uint64_t, Box>(other); }
     template <typename T> inline T get() { THORIN_UNREACHABLE; } 
@@ -25,6 +35,9 @@ private:
 
 #define THORIN_ALL_TYPE(T) T T##_;
 #include "thorin/tables/primtypetable.h"
+      int8_t  int8_t_;  int16_t  int16_t_;  int32_t  int32_t_;  int64_t  int64_t_;
+     uint8_t uint8_t_; uint16_t uint16_t_; uint32_t uint32_t_; uint64_t uint64_t_;
+     float float_; double double_;
 };
 
 static_assert(sizeof(Box) == sizeof(uint64_t), "Box has incorrect size in bytes");

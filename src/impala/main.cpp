936acb83eeb5d3490a4fbff60f47669f85a4c6ba
Roland Leißa <leissa@cs.uni-saarland.de>
2014-01-16 01:11:52 +0100

    clean up

--- src/impala/main.cpp
+++ src/impala/main.cpp
@@ -147,11 +147,9 @@ int main(int argc, char** argv) {
             if (emit_il)
                 thorin::emit_il(init.world, fancy);
             if (emit_looptree) {
-                for (auto top : top_level_lambdas(init.world)) {
-                    Scope scope(top);
-                    const LoopTree looptree(scope);
-                    std::cout << looptree.root() << std::endl; // TODO
-                }
+                Scope scope(init.world);
+                const LoopTree looptree(scope);
+                std::cout << looptree.root() << std::endl; // TODO
             }
 
             if (emit_llvm)

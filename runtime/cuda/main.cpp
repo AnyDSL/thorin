8a3e55f26767f6ddfcfda85f350424f3d43324fb
Richard Membarth <richard.membarth@uni-saarland.de>
2014-01-29 20:42:47 +0100

    Add simple test case using texture memory.

--- runtime/cuda/main.cpp
+++ runtime/cuda/main.cpp
@@ -38,6 +38,44 @@ int main_impala() {
     }
     printf("Test passed!\n");
 
+    for (unsigned int i=0; i<num; ++i) {
+        host[i] = i;
+    }
+
+
+    // CODE TO BE GENERATED: BEGIN
+    CUdeviceptr tex;
+    tex = malloc_memory(num*sizeof(int));
+    write_memory(tex, host, num*sizeof(int));
+
+    CUdeviceptr out;
+    out = malloc_memory(num*sizeof(int));
+    for (unsigned int i=0; i<num; ++i) {
+        host[i] = 0;
+    }
+    write_memory(out, host, num*sizeof(int));
+
+    load_kernel("simple-gpu64.nvvm", "simple_tex");
+    get_tex_ref("texture");
+    bind_tex(tex, CU_AD_FORMAT_SIGNED_INT32);
+    set_kernel_arg(&out);
+    set_problem_size(1024, 1, 1);
+    launch_kernel("simple");
+    synchronize(); // optional
+    read_memory(out, host, num*sizeof(int));
+    free_memory(out);
+    free_memory(tex);
+    // CODE TO BE GENERATED: END
+
+    // check result
+    for (unsigned int i=0; i<num; ++i) {
+        if (host[i] != i) {
+            printf("Texture test failed!\n");
+            return EXIT_FAILURE;
+        }
+    }
+    printf("Texture test passed!\n");
+
     return EXIT_SUCCESS;
 }
 

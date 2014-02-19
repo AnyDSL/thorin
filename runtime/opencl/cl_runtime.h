88903c0ab3b480554daca1ae231c8e8b4fe92c2b
Richard Membarth <richard.membarth@uni-saarland.de>
2014-02-19 17:42:17 +0100

    Add SPIR wrapper implementation to cpp file.

--- runtime/opencl/cl_runtime.h
+++ runtime/opencl/cl_runtime.h
@@ -27,29 +27,26 @@ void launch_kernel(const char *kernel_name);
 void synchronize();
 
 // SPIR wrappers
-inline cl_mem spir_malloc_buffer(size_t size) { return malloc_buffer(size); }
-inline void spir_free_buffer(cl_mem mem) { free_buffer(mem); }
+cl_mem spir_malloc_buffer(size_t size);
+void spir_free_buffer(cl_mem mem);
 
-inline void spir_write_buffer(cl_mem dev, void *host, size_t size) { write_buffer(dev, host, size); }
-inline void spir_read_buffer(cl_mem dev, void *host, size_t size) { read_buffer(dev, host, size); }
+void spir_write_buffer(cl_mem dev, void *host, size_t size);
+void spir_read_buffer(cl_mem dev, void *host, size_t size);
 
-inline void spir_build_program_and_kernel_from_binary(const char *file_name, const char *kernel_name) { build_program_and_kernel(file_name, kernel_name, true); }
-inline void spir_build_program_and_kernel_from_source(const char *file_name, const char *kernel_name) { build_program_and_kernel(file_name, kernel_name, false); }
+void spir_build_program_and_kernel_from_binary(const char *file_name, const char *kernel_name);
+void spir_build_program_and_kernel_from_source(const char *file_name, const char *kernel_name);
 
-inline void spir_set_kernel_arg(void *host, size_t size) { set_kernel_arg(host, size); }
-inline void spir_set_problem_size(size_t size_x, size_t size_y, size_t size_z) { set_problem_size(size_x, size_y, size_z); }
-inline void spir_set_config_size(size_t size_x, size_t size_y, size_t size_z) { set_config_size(size_x, size_y, size_z); }
+void spir_set_kernel_arg(void *host, size_t size);
+void spir_set_problem_size(size_t size_x, size_t size_y, size_t size_z);
+void spir_set_config_size(size_t size_x, size_t size_y, size_t size_z);
 
-inline void spir_launch_kernel(const char *kernel_name) { launch_kernel(kernel_name); }
-inline void spir_synchronize() { synchronize(); }
+void spir_launch_kernel(const char *kernel_name);
+void spir_synchronize();
 
+// helper functions
+float *array(size_t num_elems);
+float random_val(int max);
 extern int main_impala();
-inline float *array(size_t num_elems) {
-    return (float *)malloc(sizeof(float)*num_elems);
-}
-inline float random_val(int max) {
-    return ((float)random() / RAND_MAX) * max;
-}
 
 }
 

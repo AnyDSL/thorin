d9a3986b7a9cfa5e4894aa4f3d8714505ad85002
Roland Leißa <leissa@cs.uni-saarland.de>
2014-02-28 14:43:21 +0100

    started rewrite based on postdom

--- src/thorin/transform/partial_evaluation.cpp
+++ src/thorin/transform/partial_evaluation.cpp
@@ -3,9 +3,10 @@
 #include <unordered_map>
 #include <queue>
 
+#include "thorin/literal.h"
 #include "thorin/world.h"
 #include "thorin/analyses/scope.h"
-#include "thorin/analyses/looptree.h"
+#include "thorin/analyses/domtree.h"
 #include "thorin/be/thorin.h"
 #include "thorin/analyses/top_level_scopes.h"
 #include "thorin/transform/mangle.h"
@@ -129,241 +130,89 @@ struct CallHash {
 class PartialEvaluator {
 public:
     PartialEvaluator(World& world)
-        : world_(world)
-        , scope_(world, top_level_lambdas(world))
-        , loops_(scope_)
+        : scope_(world, top_level_lambdas(world), false)
+        , postdomtree_(scope_)
     {
-        done_.insert(nullptr);
-        loops_.dump();
-        collect_headers(loops_.root());
+        postdomtree_.dump();
         for (auto lambda : world.lambdas())
             new2old_[lambda] = lambda;
     }
 
-    void collect_headers(const LoopNode*);
-    void process();
+    World& world() { return scope().world(); }
+    const Scope& scope() const { return scope_; }
+    void seek();
+    void eval(Lambda* cur);
     void rewrite_jump(Lambda* src, Lambda* dst, const Call&);
     void update_new2old(const Def2Def& map);
-    Edge edge(Lambda* src, Lambda* dst) const;
-    TraceEntry trace_entry(Lambda* lambda) { return TraceEntry(lambda, new2old_[lambda]); }
-    TraceEntry ghost_entry() { 
-        TraceEntry entry(nullptr, nullptr); 
-        entry.todo();
-        return entry;
-    }
-    void push(Lambda* src, ArrayRef<Lambda*> dst);
-    Lambda* pop();
 
-    std::list<TraceEntry>::iterator search_loop(const Edge& edge, std::function<void(TraceEntry&)> body = [] (TraceEntry&) {}) {
-        auto i = trace_.rbegin();
-        if (edge.order() <= 0) {
-            int num = -edge.order()/2 + 1;
-            //std::cout << num << std::endl;
-            for (; num != 0; ++i) {
-                assert(i != trace_.rend());
-                if (is_header(i->nlambda())) {
-                    --num;
-                    body(*i);
-                }
-            }
-        }
-        return i.base();
-    }
-
-    const LoopHeader* is_header(Lambda* lambda) const {
-        if (lambda) {
-            auto i = lambda2header_.find(new2old_[lambda]);
-            if (i != lambda2header_.end())
-                return i->second;
-            return nullptr;
-        }
-        return loops_.root();
-    }
-
-    void dump_trace() {
-        std::cout << "*** trace ***" << std::endl;
-        for (auto entry : trace_)
-            entry.dump();
-        std::cout << "*************" << std::endl;
-    }
-
-    World& world_;
+private:
     Scope scope_;
-    LoopTree loops_;
+    const DomTree postdomtree_;
     Lambda2Lambda new2old_;
-    std::unordered_map<Lambda*, const LoopHeader*> lambda2header_;
-    std::unordered_set<Lambda*> done_;
+    LambdaSet done_;
     std::unordered_map<Call, Lambda*, CallHash> cache_;
-    std::list<TraceEntry> trace_;
 };
 
-void PartialEvaluator::push(Lambda* src, ArrayRef<Lambda*> dst) {
-    Array<Edge> edges(dst.size());
-    for (size_t i = 0, e = dst.size(); i != e; ++i)
-        edges[i] = edge(src, dst[i]);
+void PartialEvaluator::seek() {
+    std::queue<Lambda*> queue;
+    for (auto lambda : world().externals())
+        queue.push(lambda);
 
-    std::stable_sort(edges.begin(), edges.end());
+    while (!queue.empty()) {
+        auto lambda = queue.front();
+        queue.pop();
 
-    if (dst.size() > 1) {
-        for (auto d : dst)
-            if (new2old_[src]->gid() == 42 && new2old_[d]->gid() == 48) {
-                size_t num = 1;
-                auto i = trace_.rbegin();
-                for (; num != 0; ++i) {
-                    assert(i != trace_.rend());
-                    if (is_header(i->nlambda())) {
-                        --num;
-                        i->set_evil();
-                    }
-                }
-            }
-
-        for (auto& edge : edges)
-            search_loop(edge, [&] (TraceEntry& entry) { entry.set_evil(); });
-    }
-
-    for (auto& edge : edges) {
-        auto i = done_.find(edge.dst());
-        if (i != done_.end())
-            continue;
-        done_.insert(edge.dst());
-        if (edge.order() <= 0) { // more elegant here
-            auto j = search_loop(edge);
-            trace_.insert(j, trace_entry(edge.dst()));
-        } else {
-            if (edge.is_cross()) {
-                for (auto i = 0; i < edge.n(); ++i)
-                    trace_.push_back(ghost_entry());
-            }
-            trace_.push_back(trace_entry(edge.dst()));
+        if (!done_.contains(lambda)) {
+            eval(lambda);
+            for (auto succ : lambda->succs())
+                queue.push(succ);
         }
     }
 }
 
-Lambda* PartialEvaluator::pop() {
-    while (!trace_.empty() && !trace_.back().todo())
-        trace_.pop_back();
-    return trace_.empty() ? nullptr : trace_.back().nlambda();
-}
+void PartialEvaluator::eval(Lambda* cur) {
+    while (true) {
+        if (done_.contains(cur))
+            break;
+        done_.insert(cur);
+        if (cur->empty()) 
+            break;
 
-Edge PartialEvaluator::edge(Lambda* nsrc, Lambda* ndst) const {
-    auto src = new2old_[nsrc];
-    auto dst = new2old_[ndst];
-    auto hsrc = loops_.lambda2header(src);
-    auto hdst = loops_.lambda2header(dst);
+        auto to = cur->to();
+        if (auto run = to->isa<Run>())
+            to = run->def();
 
-    //std::cout << "classify: " << src->unique_name() << " -> " << dst->unique_name() << std::endl;
-    if (is_header(dst)) {
-        if (loops_.contains(hsrc, dst))
-            return Edge(nsrc, ndst, true, hdst->depth() - hsrc->depth()); // within n, n positive
-        if (loops_.contains(hdst, src))
-            return Edge(nsrc, ndst, true, hsrc->depth() - hdst->depth()); // within n, n negative
-    }
+        auto dst = to->isa_lambda();
+        if (dst == nullptr)
+            break;
 
-    //if (hdst->depth() - hsrc->depth() > 0)
-        //asm("int3");
+        Call call(dst);
+        bool fold = false;
+        for (size_t i = 0; i != cur->num_args(); ++i) {
+            call.arg(i) = cur->arg(i)->is_const() ? cur->arg(i) : nullptr;
+            fold |= call.arg(i) != nullptr; // don't fold if there is nothing to fold
+        }
 
-    return Edge(nsrc, ndst, false, hdst->depth() - hsrc->depth());        // cross n
-}
-
-void PartialEvaluator::collect_headers(const LoopNode* n) {
-    if (const LoopHeader* header = n->isa<LoopHeader>()) {
-        for (auto lambda : header->lambdas())
-            lambda2header_[lambda] = header;
-        for (auto child : header->children())
-            collect_headers(child);
-    }
-}
-
-void PartialEvaluator::process() {
-    for (auto src : top_level_lambdas(world_)) {
-        trace_.clear();
-        if (src->empty())
+        if (!fold) {
+            cur = dst;
             continue;
-        trace_.push_back(trace_entry(src));
-
-        while ((src = pop())) {
-            //std::cout << "---------------------------------" << std::endl;
-            //std::cout << "*** src: " << src->unique_name() << std::endl;
-            //dump_trace();
-            //emit_thorin(world_);
-
-            if (src->empty())
-                continue;
-            assert(!src->empty());
-
-            auto succs = src->direct_succs();
-            bool fold = false;
-
-            auto to = src->to();
-            if (auto run = to->isa<Run>()) {
-                to = run->def();
-                fold = true;
-            }
-
-            Lambda* dst = to->isa_lambda();
-
-            if (dst == nullptr) {
-                push(src, succs);
-                continue;
-            }
-
-            Call call(dst);
-
-            bool one = false;
-            for (size_t i = 0; i != src->num_args(); ++i)
-                one |= (call.arg(i) = src->arg(i)->is_const() ? src->arg(i) : nullptr) != nullptr;
-            fold &= one; // don't fold if there is nothing to fold
-
-            if (!fold) {
-                push(src, {dst});
-                continue;
-            } else {
-                if (auto header = is_header(new2old_[dst])) {
-                    auto e = edge(src, dst);
-                    //e.dump();
-                    assert(e.is_within());
-                    if (e.n() <= 0) {
-                        auto i = search_loop(e);
-                        if (!is_header(i->nlambda())->is_root()) {
-                            assert(header == is_header(i->nlambda()) && "headers don't match");
-                            if (i->is_evil())
-                                continue;
-                        }
-                    }
-                }
-            }
-
-            // check for cached version
-            auto i = cache_.find(call);
-            if (i != cache_.end())
-                rewrite_jump(src, i->second, call);
-            else {
-                // no no cached version found... create a new one
-                Scope scope(dst);
-                Def2Def old2new;
-                GenericMap generic_map;
-                bool res = dst->type()->infer_with(generic_map, src->arg_pi());
-                assert(res);
-                auto dropped = drop(scope, old2new, call.args(), generic_map);
-                //std::cout << call.to()->unique_name() << ' ';
-                //for (auto arg : call.args())
-                    //if (arg)
-                        //std::cout << arg->unique_name() << ' ';
-                    //else
-                        //std::cout << "<nulll> ";
-                //std::cout << "---" << std::endl;
-                // update call
-                old2new[dst] = dropped;
-                update_new2old(old2new);
-                rewrite_jump(src, dropped, call);
-                for (auto lambda : scope.rpo()) {
-                    auto mapped = old2new[lambda]->as_lambda();
-                    if (mapped != lambda)
-                        mapped->update_to(world_.run(mapped->to()));
-                }
-                push(src, {dropped});
-            }
+        } 
+        
+        auto i = cache_.find(call);
+        if (i != cache_.end()) {    // check for cached version
+            rewrite_jump(cur, i->second, call);
+            break;
+        } else {                    // no no cached version found... create a new one
+            Scope scope(dst);
+            Def2Def old2new;
+            GenericMap generic_map;
+            bool res = dst->type()->infer_with(generic_map, cur->arg_pi());
+            assert(res);
+            auto dropped = drop(scope, old2new, call.args(), generic_map);
+            old2new[dst] = dropped;
+            update_new2old(old2new);
+            rewrite_jump(cur, dropped, call);
+            cur = dropped;
         }
     }
 }
@@ -377,17 +226,14 @@ void PartialEvaluator::rewrite_jump(Lambda* src, Lambda* dst, const Call& call) 
 
     src->jump(dst, nargs);
     cache_[call] = dst;
-    //std::cout << "mapping:" << std::endl;
-    //call.dump();
-    //std::cout << "   to:" << std::endl;
-    //dst->dump_head();
-    //std::cout << "---" << std::endl;
 }
 
 void PartialEvaluator::update_new2old(const Def2Def& old2new) {
     for (auto p : old2new) {
         if (auto olambda = p.first->isa_lambda()) {
             auto nlambda = p.second->as_lambda();
+            if (!nlambda->empty() && nlambda->to()->isa<Bottom>())
+                continue;
             //std::cout << nlambda->unique_name() << " -> "  << olambda->unique_name() << std::endl;
             assert(new2old_.contains(olambda));
             new2old_[nlambda] = new2old_[olambda];
@@ -398,7 +244,7 @@ void PartialEvaluator::update_new2old(const Def2Def& old2new) {
 //------------------------------------------------------------------------------
 
 void partial_evaluation(World& world) { 
-    PartialEvaluator(world).process(); 
+    PartialEvaluator(world).seek(); 
 
     for (auto primop : world.primops()) {
         if (auto evalop = primop->isa<EvalOp>())

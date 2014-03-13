1d48e9efa3f68419318db4011e78e1cc609c8a93
Richard Membarth <richard.membarth@uni-saarland.de>
2014-03-13 16:22:30 +0100

    Intel OpenCL optimization guide: Also allocation should be sized as a multiple of cache lines (64 bytes).

--- runtime/cuda/cu_runtime.cpp
+++ runtime/cuda/cu_runtime.cpp
@@ -503,7 +503,7 @@ void nvvm_synchronize(size_t dev) { synchronize(dev); }
 // helper functions
 void *array(size_t elem_size, size_t width, size_t height) {
     void *mem;
-    posix_memalign(&mem, 16, elem_size * width * height);
+    posix_memalign(&mem, 64, elem_size * width * height);
     std::cerr << " * array() -> " << mem << std::endl;
     host_mems_[mem] = {elem_size, width, height};
     return mem;

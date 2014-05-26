2107b255185ea9ae37c8adf5b19fc6615ba2fa8a
Roland Leißa <leissa@cs.uni-saarland.de>
2014-05-26 14:58:03 +0200

    removed benchmarks

--- runtime/benchmarks/build.sh
+++
@@ -1,5 +0,0 @@
-gcc support.c -c -O2 -mavx -Wall
-./impala extern.impala mapping_cpu.impala gaussian.impala -emit-llvm
-opt -O3 gaussian.bc -o gaussian_opt.bc
-llc gaussian_opt.bc -mattr=avx
-gcc gaussian_opt.s support.o

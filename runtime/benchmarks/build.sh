8499463bed471d94668c1f71a829252d571c63c3
Roland Leißa <leissa@cs.uni-saarland.de>
2014-05-26 14:56:38 +0200

    removing benchmarks

--- runtime/benchmarks/build.sh
+++
@@ -1,6 +0,0 @@
-gcc support.c -c -O2 -mavx -Wall
-./impala extern.impala mapping_cpu.impala $1 -emit-llvm
-base=${1%.*}
-opt -O3 ${base}.bc -o ${base}_opt.bc
-llc ${base}_opt.bc -mattr=avx
-gcc ${base}_opt.s support.o

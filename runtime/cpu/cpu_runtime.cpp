4a10e553bcbafc9f08d73e47317186f6759e5277
Ralf Jung <post@ralfj.de>
2014-07-22 14:23:32 +0200

    Print size in mallocs

--- runtime/cpu/cpu_runtime.cpp
+++ runtime/cpu/cpu_runtime.cpp
@@ -8,7 +8,7 @@ void thorin_init() { }
 void *thorin_malloc(size_t size) {
     void *mem;
     posix_memalign(&mem, 64, size);
-    std::cerr << " * malloc() -> " << mem << std::endl;
+    std::cerr << " * malloc(" << size << ") -> " << mem << std::endl;
     return mem;
 }
 void thorin_free(void *ptr) {

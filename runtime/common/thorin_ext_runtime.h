9384bbc1683038c9367740bafc6f29aeede0ad60
Ralf Jung <post@ralfj.de>
2014-07-24 10:41:51 +0200

    runtime: When in C++, provide thorin_new

--- runtime/common/thorin_ext_runtime.h
+++ runtime/common/thorin_ext_runtime.h
@@ -1,7 +1,11 @@
 #ifndef _THORIN_EXT_RUNTIME_H
 #define _THORIN_EXT_RUNTIME_H
 
+#ifdef __cplusplus
+#include <cstdlib>
+#else
 #include <stdlib.h>
+#endif
 
 extern "C" {
     // runtime functions, used externally from C++ interface
@@ -9,6 +13,14 @@ extern "C" {
     void *thorin_malloc(size_t size);
     void thorin_free(void *ptr);
     void thorin_print_total_timing();
-} 
+}
+
+#ifdef __cplusplus
+template<typename T>
+static T* thorin_new(unsigned n)
+{
+    return (T*)thorin_malloc(n*sizeof(T));
+}
+#endif
 
 #endif

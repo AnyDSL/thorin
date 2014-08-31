ad4b2358870ea7ef14f5cddd70041e83d290d1b8
Roland Leißa <leissa@cs.uni-saarland.de>
2014-09-01 01:55:12 +0200

    tweaking bb schedule

--- src/thorin/analyses/bb_schedule.cpp
+++ src/thorin/analyses/bb_schedule.cpp
@@ -10,25 +10,40 @@ static int count_children(const DomNode* n, LambdaMap<int>& lambda2num) {
     return (lambda2num[n->lambda()] = num) + 1;
 }
 
-static void bb_schedule(const DomNode* n, const LoopTree& looptree, std::vector<Lambda*>& bbs, const LambdaMap<int>& lambda2num) {
-    bbs.push_back(n->lambda());
+static void bb_schedule(const Scope& scope, const DomNode* n, const LoopTree& looptree,
+                        std::vector<Lambda*>& bbs, const LambdaMap<int>& lambda2num) {
+    auto lambda = n->lambda();
+    bbs.push_back(lambda);
     auto children = n->children();
     std::sort(children.begin(), children.end(), [&] (const DomNode* n1, const DomNode* n2) {
         auto l1 = n1->lambda();
         auto l2 = n2->lambda();
-        auto d1 = looptree.depth(l1);
-        auto d2 = looptree.depth(l2);
-        if (d1 > d2) return true;
-        if (d1 < d2) return false;
-        auto i1 = lambda2num.find(l1)->second;
-        auto i2 = lambda2num.find(l2)->second;
-        if (i1 > i2) return true;
-        if (i1 < i2) return false;
+
+        // handle loops first
+        auto depth1 = looptree.depth(l1);
+        auto depth2 = looptree.depth(l2);
+        if (depth1 > depth2) return true;
+        if (depth1 < depth2) return false;
+
+        // if this fails - use the one with less children in the domtree
+        auto num1 = lambda2num.find(l1)->second;
+        auto num2 = lambda2num.find(l2)->second;
+        if (num1 < num2) return true;
+        if (num1 > num2) return false;
+
+        // if this fails use the one which is a direct succ of lambda
+        const auto& succs = scope.succs(lambda);
+        auto is_succ1 = std::find(succs.begin(), succs.end(), l1) != succs.end();
+        auto is_succ2 = std::find(succs.begin(), succs.end(), l2) != succs.end();
+        if ( is_succ1 && !is_succ2) return true;
+        if (!is_succ1 &&  is_succ2) return false;
+
+        // if this still fails - simply use smaller gid
         return l1->gid() < l2->gid();
     });
 
     for (auto child : children)
-        bb_schedule(child, looptree, bbs, lambda2num);
+        bb_schedule(scope, child, looptree, bbs, lambda2num);
 }
 
 std::vector<Lambda*> bb_schedule(const Scope& scope) {
@@ -37,7 +52,7 @@ std::vector<Lambda*> bb_schedule(const Scope& scope) {
     LambdaMap<int> lambda2num;
     count_children(domtree.root(), lambda2num);
     std::vector<Lambda*> bbs;
-    bb_schedule(domtree.root(), looptree, bbs, lambda2num);
+    bb_schedule(scope, domtree.root(), looptree, bbs, lambda2num);
     assert(bbs.size() == scope.size());
     return bbs;
 }

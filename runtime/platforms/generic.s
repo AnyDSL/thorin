d1a409a4ea5f05b2fc9d895fbd65cedf0650977e
Richard Membarth <richard.membarth@uni-saarland.de>
2014-09-01 12:29:33 +0200

    Update signature of munmap.

--- runtime/platforms/generic.s
+++ runtime/platforms/generic.s
@@ -3,7 +3,7 @@
 declare i8* @thorin_malloc(i64);
 declare void @thorin_free(i8*);
 declare i64 @map_memory(i32, i32, i8*, i32, i32);
-declare void @unmap_memory(i32, i32, i64);
+declare void @unmap_memory(i64);
 
 declare i64 @parallel_create(i32, i8*, i64, i8*);
 declare void @parallel_join(i64);

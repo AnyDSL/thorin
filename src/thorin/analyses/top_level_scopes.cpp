52e77d5a30f8f745ff3d80bba8972944fda77a21
Roland Leißa <leissa@cs.uni-saarland.de>
2014-09-17 20:22:21 +0200

    implemented alternative top_level_scopes

--- src/thorin/analyses/top_level_scopes.cpp
+++ src/thorin/analyses/top_level_scopes.cpp
@@ -1,6 +1,7 @@
 #include "thorin/analyses/top_level_scopes.h"
 
 #include "thorin/world.h"
+#include "thorin/util/queue.h"
 
 namespace thorin {
 
@@ -8,14 +9,14 @@ AutoVector<Scope*> top_level_scopes(World& world) {
     AutoVector<Scope*> scopes;
     LambdaSet done;
 
-    auto insert = [&] (Lambda* lambda) { 
+    auto insert = [&] (Lambda* lambda) {
         if (done.contains(lambda))
             return;
         auto scope = new Scope(lambda);
         scopes.emplace_back(scope);
 
         for (auto lambda : *scope)
-            done.insert(lambda); 
+            done.insert(lambda);
     };
 
     for (auto lambda : world.externals())
@@ -35,6 +36,31 @@ AutoVector<Scope*> top_level_scopes(World& world) {
     }
 
     return scopes;
+}
+
+void top_level_scopes(World& world, std::function<void(Scope&)> f, bool elide_empty) {
+    LambdaSet done;
+    std::queue<Lambda*> queue;
+
+    for (auto lambda : world.externals())
+        queue.push(lambda);
+
+    while (!queue.empty()) {
+        auto lambda = pop(queue);
+        if (elide_empty && lambda->empty())
+            continue;
+        Scope scope(lambda);
+        f(scope);
+        for (auto lambda : scope)
+            done.insert(lambda);
+
+        for (auto lambda : scope) {
+            for (auto succ : lambda->succs()) {
+                if (!done.contains(succ))
+                    queue.push(succ);
+            }
+        }
+    }
 }
 
 }
--- src/thorin/analyses/top_level_scopes.h
+++ src/thorin/analyses/top_level_scopes.h
@@ -1,12 +1,15 @@
-#ifndef THORIN_ANALYSES_TOP_LEVEL_H
-#define THORIN_ANALYSES_TOP_LEVEL_H
+#ifndef THORIN_ANALYSES_TOP_LEVEL_SCOPES_H
+#define THORIN_ANALYSES_TOP_LEVEL_SCOPES_H
 
-#include "thorin/analyses/scope.h"
+#include <functional>
+
 #include "thorin/util/autoptr.h"
+#include "thorin/analyses/scope.h"
 
 namespace thorin {
 
 AutoVector<Scope*> top_level_scopes(World&);
+void top_level_scopes(World&, std::function<void(Scope&)>, bool elide_empty = true);
 
 }
 

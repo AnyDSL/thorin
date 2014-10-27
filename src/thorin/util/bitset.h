ab14d83a6168f1078f5ad5c5f42d1c655028662a
Roland Leißa <leissa@cs.uni-saarland.de>
2014-10-27 18:15:50 +0100

    more features in BitSet

--- src/thorin/util/bitset.h
+++ src/thorin/util/bitset.h
@@ -31,22 +31,55 @@ public:
         size_t pos_;
     };
 
-    BitSet(size_t size)
+    BitSet(size_t size = 0)
         : size_(size)
         , bits_(size / 64u + 1u)
     {}
+    BitSet(BitSet&& other)
+        : BitSet()
+    {
+        swap(*this, other);
+    }
+    BitSet(const BitSet& other)
+        : size_(other.size_)
+        , bits_(other.bits_)
+    {}
 
     size_t size() const { return size_; }
-    reference operator[] (size_t i) {
-        assert(i < size_ && "out of bounds access");
-        return reference(bits_[i / 64u], i % 64u);
-    }
     size_t next(size_t pos = 0) {
         for (size_t i = pos, e = size(); i != e; ++i) {
             if (bits_[i])
                 return i;
         }
         return pos;
+    }
+    reference operator[] (size_t i) {
+        assert(i < size_ && "out of bounds access");
+        return reference(bits_[i / 64u], i % 64u);
+    }
+    BitSet& operator |= (const BitSet& other) {
+        assert(this->size() == other.size());
+        for (size_t i = 0, e = size(); i != e; ++i)
+            this->bits_[i] |= other.bits_[i];
+        return *this;
+    }
+    BitSet& operator &= (const BitSet& other) {
+        assert(this->size() == other.size());
+        for (size_t i = 0, e = size(); i != e; ++i)
+            this->bits_[i] &= other.bits_[i];
+        return *this;
+    }
+    BitSet& operator ^= (const BitSet& other) {
+        assert(this->size() == other.size());
+        for (size_t i = 0, e = size(); i != e; ++i)
+            this->bits_[i] |= other.bits_[i];
+        return *this;
+    }
+    BitSet& operator = (BitSet other) { swap(*this, other); return *this; }
+    friend void swap(BitSet set1, BitSet set2) {
+        using std::swap;
+        swap(set1.size_, set2.size_);
+        swap(set1.bits_, set2.bits_);
     }
 
 private:

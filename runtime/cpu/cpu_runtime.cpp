770835debd4197e60acb528a92b36f981172f2f6
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2015-02-17 16:28:35 +0100

    CPU runtime tabs to spaces

--- runtime/cpu/cpu_runtime.cpp
+++ runtime/cpu/cpu_runtime.cpp
@@ -20,28 +20,28 @@ void thorin_free(void *ptr) {
 void thorin_print_total_timing() { }
 
 void parallel_for(int num_threads, int lower, int upper, void *args, uint64_t arg_size, void *fun) {
-	// C++11 threads version
-	void (*fun_ptr) (void*) = reinterpret_cast<void (*) (void*)>(fun);
-	const int count = upper - lower;
-	const int linear = count / num_threads;
-	const int residual = linear + count % num_threads;
+    // C++11 threads version
+    void (*fun_ptr) (void*) = reinterpret_cast<void (*) (void*)>(fun);
+    const int count = upper - lower;
+    const int linear = count / num_threads;
+    const int residual = linear + count % num_threads;
 
-	// Create a pool of threads to execute the task
-	std::vector<std::thread> pool(num_threads);
+    // Create a pool of threads to execute the task
+    std::vector<std::thread> pool(num_threads);
 
-	for (int i = 0; i < num_threads - 1; i++) {
-		pool[i] = std::thread([args, fun_ptr, linear]() {
-			for (int j = 0; j < linear; j++)
-				fun_ptr(args);
-		});
-	}
+    for (int i = 0; i < num_threads - 1; i++) {
+        pool[i] = std::thread([args, fun_ptr, linear]() {
+            for (int j = 0; j < linear; j++)
+                fun_ptr(args);
+        });
+    }
 
-	pool[num_threads - 1] = std::thread([args, fun_ptr, residual]() {
-		for (int j = 0; j < residual; j++)
-			fun_ptr(args);
-	});
+    pool[num_threads - 1] = std::thread([args, fun_ptr, residual]() {
+        for (int j = 0; j < residual; j++)
+            fun_ptr(args);
+    });
 
-	// Wait for all the threads to finish
-	for (int i = 0; i < num_threads; i++)
-		pool[i].join();
+    // Wait for all the threads to finish
+    for (int i = 0; i < num_threads; i++)
+        pool[i].join();
 }

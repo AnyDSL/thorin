d29cf96f81558ffcfb65f55fb1d15892009a25fe
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2015-02-18 18:23:42 +0100

    Added range parameters to parallel for runtime

--- runtime/cpu/cpu_runtime.cpp
+++ runtime/cpu/cpu_runtime.cpp
@@ -6,6 +6,10 @@
 #include "cpu_runtime.h"
 #include "thorin_runtime.h"
 
+#ifdef USE_TBB
+#include "tbb/parallel_for.h"
+#endif
+
 // helper functions
 void thorin_init() { }
 void *thorin_malloc(uint32_t size) {
@@ -19,29 +23,37 @@ void thorin_free(void *ptr) {
 }
 void thorin_print_total_timing() { }
 
-void parallel_for(int num_threads, int lower, int upper, void *args, uint64_t arg_size, void *fun) {
+#ifndef USE_TBB
+void parallel_for(int num_threads, int lower, int upper, void *args, void *fun) {
     // C++11 threads version
-    void (*fun_ptr) (void*) = reinterpret_cast<void (*) (void*)>(fun);
-    const int count = upper - lower;
-    const int linear = count / num_threads;
-    const int residual = linear + count % num_threads;
+    void (*fun_ptr) (void*, int, int) = reinterpret_cast<void (*) (void*, int, int)>(fun);
+    const int linear = (upper - lower) / num_threads;
 
     // Create a pool of threads to execute the task
     std::vector<std::thread> pool(num_threads);
 
-    for (int i = 0; i < num_threads - 1; i++) {
-        pool[i] = std::thread([args, fun_ptr, linear]() {
-            for (int j = 0; j < linear; j++)
-                fun_ptr(args);
+    for (int i = 0, a = lower, b = lower + linear; i < num_threads - 1; a = b, b += linear, i++) {
+        pool[i] = std::thread([=]() {
+            fun_ptr(args, a, b);
         });
     }
 
-    pool[num_threads - 1] = std::thread([args, fun_ptr, residual]() {
-        for (int j = 0; j < residual; j++)
-            fun_ptr(args);
+    pool[num_threads - 1] = std::thread([=]() {
+        fun_ptr(args, lower + (num_threads - 1) * linear, upper);
     });
 
     // Wait for all the threads to finish
     for (int i = 0; i < num_threads; i++)
         pool[i].join();
 }
+#else
+void parallel_for(int num_threads, int lower, int upper, void *args, uint64_t arg_size, void *fun) {
+    // TBB version
+    void (*fun_ptr) (void*, int, int) = reinterpret_cast<void (*) (void*, int, int)>(fun);
+
+    tbb::parallel_for(lower, upper, [] (const blocked_range<int>& range) {
+        fun_ptr(args, range.begin(), range.end());
+    });
+}
+#endif
+

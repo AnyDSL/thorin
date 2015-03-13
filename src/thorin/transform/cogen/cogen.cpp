35457c981f05c107242f59782c3e2c6de09fb0b6
Immanuel Haffner <haffner@cs.uni-saarland.de>
2015-03-13 14:11:47 +0100

    started implementing CoGen::emit

--- src/thorin/transform/cogen/cogen.cpp
+++ src/thorin/transform/cogen/cogen.cpp
@@ -1,12 +1,63 @@
 #include "thorin/transform/cogen/cogen.h"
 
-#include "thorin/analyses/bta.h"
+#include <string>
+#include "thorin/analyses/scope.h"
+
+namespace {
+
+std::string toCType(thorin::Type t) {
+    // TODO: do something meaningful
+    return "int";
+}
+
+}
 
 namespace thorin {
 
 void CoGen::run(World &world) {
-    BTA bta;
     bta.run(world);
+
+    emit_head(std::cout);
+
+    Scope::for_each(world, [&](Scope const & s){
+            emit_generator(std::cout, s.entry());
+    });
+}
+
+void CoGen::emit_generator(std::ostream &out, Lambda *lambda) {
+    auto name = lambda->unique_name() + "_gen";
+    std::vector<Param const *> static_params;
+    std::vector<Param const *> dynamic_params;
+
+    /* Distribute params into static and dynamic. */
+    for (auto param : lambda->params()) {
+        if (bta.get(param).isTop())
+            dynamic_params.push_back(param);
+        else
+            static_params.push_back(param);
+    }
+
+    out << "Lambda * " << name << "(";
+    out << "World &world";
+    for (auto param : static_params) {
+        out << ", " << toCType(param->type()) << " " << param->name;
+    }
+    out << ") {\n";
+
+    // TODO: emit body
+
+    out << "}\n";
+}
+
+void CoGen::emit_head(std::ostream &out) {
+    out
+        << "#include \"thorin/def.h\"\n"
+        << "#include \"thorin/world.h\"\n"
+        << "#include \"thorin/be/thorin.h\"\n"
+        << "\n"
+        << "using namespace thorin;\n"
+        << "\n"
+        ;
 }
 
 }
--- src/thorin/transform/cogen/cogen.h
+++ src/thorin/transform/cogen/cogen.h
@@ -1,21 +1,29 @@
 #ifndef THORIN_TRANSFORM_COGEN_H
 #define THORIN_TRANSFORM_COGEN_H
 
+#include <iostream>
 #include "thorin/def.h"
+#include "thorin/analyses/bta.h"
+#include "thorin/type.h"
+#include "thorin/lambda.h"
 
 namespace thorin {
 
 struct CoGen {
     void run(World &world);
-
+    void emit_generator(std::ostream &out, Lambda *lambda);
 
     private:
+    BTA bta;
+
+    void emit_head(std::ostream &out);
+
     /* Emit staged code. */
-    void emit_lambda();
-    void emit_select();
-    void emit_jump();
-    void emit_add();
-    void emit_mul();
+    void create_lambda();
+    void create_select();
+    void create_jump();
+    void create_add();
+    void create_mul();
 };
 
 }

8b1836b268d15e3344e7765403c54a0d27f9b1da
Richard Membarth <richard.membarth@uni-saarland.de>
2015-04-29 18:43:20 +0200

    Cleanup.

--- runtime/cuda/cu_test_gen.cpp
+++ runtime/cuda/cu_test_gen.cpp
@@ -5,7 +5,7 @@
 
 #include "thorin_runtime.h"
 
-static void nnvm_load_any_kernel(uint32_t dev, const char* file, const char* kernel) {
+static void nvvm_load_any_kernel(uint32_t dev, const char* file, const char* kernel) {
     size_t len = strlen(file);
     if (len > 3 && !strcmp(file+len-3, ".cu")) // hack: nvcc checks for .cu extension, so we just do the same here
         nvvm_load_cuda_kernel(dev, file, kernel);
@@ -28,7 +28,7 @@ int test_kernelfile(const char* file) {
     for (size_t i=0; i<num; ++i) host[i] = 0;
     mem_id mem = nvvm_malloc_memory(dev, host);
     nvvm_write_memory(dev, mem, host);
-    nnvm_load_any_kernel(dev, file, "simple");
+    nvvm_load_any_kernel(dev, file, "simple");
     nvvm_set_kernel_arg_map(dev, mem);
     nvvm_set_problem_size(dev, 1024, 1, 1);
     nvvm_set_config_size(dev, 128, 1, 1);
@@ -56,7 +56,7 @@ int test_kernelfile(const char* file) {
     nvvm_write_memory(dev, tex, host);
     CUdeviceptr out = nvvm_malloc_memory(dev, host_out);
     nvvm_write_memory(dev, out, host_out);
-    nnvm_load_any_kernel(dev, file, "simple_tex");
+    nvvm_load_any_kernel(dev, file, "simple_tex");
     nvvm_set_kernel_arg_tex(dev, tex, "tex", CU_AD_FORMAT_SIGNED_INT32);
     nvvm_set_kernel_arg_map(dev, out);
     nvvm_set_problem_size(dev, 1024, 1, 1);
@@ -84,7 +84,7 @@ int test_kernelfile(const char* file) {
     for (size_t i=0; i<num; ++i) host[i] = 0;
     mem = nvvm_malloc_memory(dev, host);
     nvvm_write_memory(dev, mem, host);
-    nnvm_load_any_kernel(dev, file, "simple_cmem");
+    nvvm_load_any_kernel(dev, file, "simple_cmem");
     nvvm_set_kernel_arg_map(dev, mem);
     nvvm_set_kernel_arg_const(dev, cmem, "cmem", sizeof(int) * 32);
     nvvm_set_problem_size(dev, 1024, 1, 1);

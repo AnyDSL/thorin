dba5c807f2e46f05da01078e8678a7290c3fc190
Immanuel Haffner <haffner.immanuel@gmail.com>
2015-06-09 00:45:51 +0200

    WIP: implementing DFG construction

--- src/thorin/analyses/dfg.cpp
+++ src/thorin/analyses/dfg.cpp
@@ -20,6 +20,26 @@ void DFGBase<forward>::Node::dump() const {
 
 template<bool forward>
 void DFGBase<forward>::create() {
+    auto &domtree = cfg().domtree();
+
+    for (auto n : cfg().rpo())
+        nodes_.emplace(nodes_.end(), n);
+
+    // compute the dominance frontier of each node as described in Cooper et al.
+    for (auto n : cfg().body()) {
+        const auto &preds = cfg().preds(n);
+        if (preds.size() > 1) {
+            auto idom = domtree[n]->idom()->cf_node();
+            for (auto pred : preds) {
+                auto runner = pred;
+                while (runner != idom) {
+                    auto domrunner = domtree[runner];
+                    /* TODO: add DFG edge */
+                    runner = domrunner->idom()->cf_node();
+                }
+            }
+        }
+    }
 }
 
 template<bool forward>

796f182c6105b05947e782ad804782f006ca4baa
Richard Membarth <richard.membarth@uni-saarland.de>
2015-06-22 11:34:21 +0200

    Patch emitted llvm 3.6 IR for NVVM & SPIR.

--- runtime/post-patcher.py
+++ runtime/post-patcher.py
@@ -30,9 +30,25 @@ if rttype in ("nvvm", "spir"):
                     print("Removing attribute declarations")
                     # ignore this line
                     pass
+                # emit correct datalayout for llvm 3.6
+                elif 'target datalayout = "e-i32' in line:
+                    if rttype=="spir":
+                        result.append('target datalayout = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v24:32:32-v32:32:32-v48:64:64-v64:64:64-v96:128:128-v128:128:128-v192:256:256-v256:256:256-v512:512:512-v1024:1024:1024"\n')
+                    else:
+                        result.append('target datalayout = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-v128:128:128-n16:32:64"\n')
+                elif 'target datalayout = "e-i64' in line:
+                    if rttype=="spir":
+                        result.append('target datalayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v24:32:32-v32:32:32-v48:64:64-v64:64:64-v96:128:128-v128:128:128-v192:256:256-v256:256:256-v512:512:512-v1024:1024:1024"\n')
+                    else:
+                        result.append('target datalayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-v128:128:128-n16:32:64"\n')
                 # it's a normal line, keep it but apply substitutions
                 else:
+                    # get rid of attributes at end of line
                     line = re.sub('#[0-9]+', '', line)
+                    # add metadata for llvm 3.6
+                    line = re.sub(r'\A!([0-9]+) = !', r'!\1 = metadata !', line)
+                    line = re.sub(r', !"kernel",', r', metadata !"kernel",', line)
+                    line = re.sub(r', !"texture",', r', metadata !"texture",', line)
                     result.append(line)
         # we have the patched thing, write it
         with open(filename, "w") as f:

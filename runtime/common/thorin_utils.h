f54ba493b9a8cc8286f56ffe9c0318322284661e
Richard Membarth <richard.membarth@uni-saarland.de>
2015-07-23 22:52:49 +0200

    Use correct function name.

--- runtime/common/thorin_utils.h
+++ runtime/common/thorin_utils.h
@@ -8,7 +8,7 @@ extern "C" {
     long long thorin_get_kernel_time();
     void thorin_print_char(char);
     void thorin_print_int(int);
-    void thorin_print_longlong(long long);
+    void thorin_print_long(long long);
     void thorin_print_float(float);
     void thorin_print_double(double);
     void* thorin_aligned_malloc(size_t, size_t);

1948077faffa456895e80b21f62dc2dde99ada7e
Richard Membarth <richard.membarth@uni-saarland.de>
2015-09-02 14:53:31 +0000

    Use std::chrono on GNU/Linux and Mac OS X.

--- runtime/common/thorin_utils.cpp
+++ runtime/common/thorin_utils.cpp
@@ -1,17 +1,11 @@
-#include <cstdlib>
+#include <atomic>
 #include <chrono>
-#include <ctime>
+#include <cstdlib>
 #include <iostream>
 #include <random>
-#include <atomic>
 
 #if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__)))
 #include <unistd.h>
-#endif
-
-#ifdef __APPLE__
-#include <mach/clock.h>
-#include <mach/mach.h>
 #endif
 
 #if defined(_WIN32) || defined(__CYGWIN__)
@@ -47,20 +41,7 @@ long long thorin_get_micro_time() {
     QueryPerformanceFrequency(&freq);
     return counter.QuadPart * 1000000LL / freq.QuadPart;
 #else
-    struct timespec now;
-#ifdef __APPLE__ // OS X does not have clock_gettime, use clock_get_time
-    clock_serv_t cclock;
-    mach_timespec_t mts;
-    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-    clock_get_time(cclock, &mts);
-    mach_port_deallocate(mach_task_self(), cclock);
-    now.tv_sec = mts.tv_sec;
-    now.tv_nsec = mts.tv_nsec;
-#else
-    clock_gettime(CLOCK_MONOTONIC, &now);
-#endif
-    long long time = now.tv_sec * 1000000LL + now.tv_nsec / 1000LL;
-    return time;
+    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 #endif
 }
 

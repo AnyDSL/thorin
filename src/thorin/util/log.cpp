d8013c2d4cc3fc060338c365ac0e50064fa991e3
Roland Leißa <leissa@cs.uni-saarland.de>
2015-10-01 18:01:36 +0200

    polish logging output

--- src/thorin/util/log.cpp
+++ src/thorin/util/log.cpp
@@ -1,5 +1,7 @@
 #include "log.h"
 
+#include <iomanip>
+
 #include "vstreamf.h"
 
 namespace thorin {
@@ -9,7 +11,8 @@ std::ostream* Log::stream_ = nullptr;
 
 void Log::log(Log::Level level, const char* file, int line, const char* fmt, ...) {
 	if (Log::stream_ && level <= Log::max_level()) {
-        Log::stream() << (level == Log::Info ? "I:" : "D:") << file << ':' << line << ": ";
+        Log::stream() << (level == Log::Info ? "I:" : "D:") << file << ':' 
+                      << std::setw(4) << std::setfill('0') << line << ": ";
 		va_list ap;
 		va_start(ap, fmt);
 		vstreamf(Log::stream(), fmt, ap);

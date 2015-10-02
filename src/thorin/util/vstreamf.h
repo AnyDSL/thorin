d941ec8476b881c4eb027147a0e022e9ce6a2bbb
Roland Leißa <leissa@cs.uni-saarland.de>
2015-10-02 02:41:16 +0200

    added doxygen

--- src/thorin/util/vstreamf.h
+++ src/thorin/util/vstreamf.h
@@ -6,11 +6,19 @@
 
 namespace thorin {
 
+/** 
+ * @brief vfprintf-like functions which works on a C++-stream.
+ * Additionally, vstreamf allows the following conversion specifiers:
+ * * Use '%S' for a @p std::string.
+ * * Use '%Y' for a @p Streamable*.
+ */
 void vstreamf(std::ostream& out, char const* fmt, va_list ap);
 
+/// Inherit from this class and implement @p stream in order to use the '%Y' conversion specifier of @p vstreamf.
 class Streamable {
 public:
     virtual std::ostream& stream(std::ostream&) const = 0;
+    /// Uses @p stream in order to dump to @p std::cout.
     void dump() const;
 };
 

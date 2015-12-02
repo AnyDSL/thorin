4c5180e0835cbae20503690c611aa0b77e4b1c92
Roland Leißa <leissa@cs.uni-saarland.de>
2015-12-02 18:51:12 +0100

    Revert "switch between EvalOp variant in CFA"
    This reverts commit 5c92791299863ff18c86317b79aa37cc5b0d1337.

--- src/thorin/analyses/cfg.cpp
+++ src/thorin/analyses/cfg.cpp
@@ -195,16 +195,8 @@ void CFABuilder::propagate_higher_order_values() {
                     stack.pop();
                 } else {
                     bool todo = false;
-#if 0
-                    if (auto evalop = def->isa<EvalOp>()) { // ignore evalop's end
-                        todo |= push(evalop->begin());
-                    } else {
-#else
-                    {
-#endif
-                        for (auto op : def->as<PrimOp>()->ops())
-                            todo |= push(op);
-                    }
+                    for (auto op : def->as<PrimOp>()->ops())
+                        todo |= push(op);
                     if (!todo) {
                         for (auto op : def->as<PrimOp>()->ops())
                             set.insert_range(def2set_[op]);

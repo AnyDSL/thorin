019f544eef2ce2a93524135f831cb3ade010115c
Roland Leißa <leissa@cs.uni-saarland.de>
2016-02-23 16:12:47 +0100

    bug fix in inliner

--- src/thorin/transform/inliner.cpp
+++ src/thorin/transform/inliner.cpp
@@ -11,9 +11,11 @@ void inliner(World& world) {
     Scope::for_each(world, [] (const Scope& scope) {
         for (auto n : scope.f_cfg().post_order()) {
             auto lambda = n->lambda();
-            if (auto to_lambda = n->lambda()) {
-                if (to_lambda->num_uses() <= 2 && !scope.outer_contains(to_lambda))
-                    lambda->jump(drop(scope, lambda->type_args(), lambda->args()), {}, {}, lambda->jump_loc());
+            if (auto to_lambda = lambda->to()->isa_lambda()) {
+                if (!to_lambda->empty() && to_lambda->num_uses() <= 2 && !scope.outer_contains(to_lambda)) {
+                    Scope to_scope(to_lambda);
+                    lambda->jump(drop(to_scope, lambda->type_args(), lambda->args()), {}, {}, lambda->jump_loc());
+                }
             }
         }
     });

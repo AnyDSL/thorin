1f4507d161ff308f1562c4e3de86389ae12f35d1
Roland Leißa <leissa@cs.uni-saarland.de>
2016-02-23 22:12:09 +0100

    more work on inliner

--- src/thorin/transform/inliner.cpp
+++ src/thorin/transform/inliner.cpp
@@ -8,19 +8,17 @@
 namespace thorin {
 
 void inliner(World& world) {
-#if 0
     Scope::for_each(world, [] (const Scope& scope) {
         for (auto n : scope.f_cfg().post_order()) {
             auto lambda = n->lambda();
             if (auto to_lambda = lambda->to()->isa_lambda()) {
-                if (!to_lambda->empty() && to_lambda->num_uses() <= 2 && !scope.outer_contains(to_lambda)) {
+                if (!to_lambda->empty() && to_lambda->num_uses() <= 1 && !scope.outer_contains(to_lambda)) {
                     Scope to_scope(to_lambda);
                     lambda->jump(drop(to_scope, lambda->type_args(), lambda->args()), {}, {}, lambda->jump_loc());
                 }
             }
         }
     });
-#endif
 
     debug_verify(world);
 }

483fe3546de03394bf5d31924adc612862d31050
Roland Leißa <leissa@cs.uni-saarland.de>
2016-03-01 16:33:27 +0100

    started new scheduler

--- src/thorin/analyses/schedule.cpp
+++ src/thorin/analyses/schedule.cpp
@@ -54,42 +54,70 @@ void Schedule::verify() {
 #endif
 }
 
-static Def2CFNode schedule_early(const Scope& scope) {
-    Def2CFNode def2early;
-    DefMap<int> def2num;
-    auto& cfg = scope.f_cfg();
+typedef std::set<Use, UseLT> Uses;
+
+DefMap<Uses> local_uses(const Scope& scope) {
+    DefMap<Uses> def2uses;
     std::queue<Def> queue;
+    DefSet done;
 
-    for (auto def : scope.in_scope()) {
-        if (auto primop = def->isa<PrimOp>()) {
-            int num = 0;
-            for (auto op : primop->ops()) {
-                if (scope._contains(op))
-                    ++num;
-            }
-            def2num[def] = num;
-        }
-    }
-
-    auto enqueue_uses = [&] (Def def) {
-        for (auto use : def->uses()) {
-            if (auto primop = use->isa<PrimOp>()) {
-                if (scope._contains(primop)) {
-                    if (--def2num[primop] == 0)
-                        queue.push(primop);
-                }
-            }
+    auto enqueue = [&](Def def, size_t i, Def op) {
+        if (scope._contains(op)) {
+            auto p1 = def2uses[op].emplace(i, def);
+            assert_unused(p1.second);
+            auto p2 = done.insert(op);
+            if (p2.second)
+                queue.push(op);
         }
     };
+
+    for (auto n : scope.f_cfg().reverse_post_order()) {
+        queue.push(n->lambda());
+        auto p = done.insert(n->lambda());
+        assert_unused(p.second);
+    }
+
+    while (!queue.empty()) {
+        auto def = pop(queue);
+        for (size_t i = 0, e = def->size(); i != e; ++i)
+            enqueue(def, i, def->op(i));
+    }
+
+    return def2uses;
+}
+
+static Def2CFNode schedule_early(const Scope& scope) {
+    Def2CFNode def2early;
+    DefMap<int> def2num_ops;
+    auto def2uses = local_uses(scope);
+
+    for (const auto& p : def2uses) {
+        auto def = p.first;
+        int num_ops = 0;
+        for (auto op : def->ops()) {
+            if (def2uses.find(op) != def2uses.end())
+                ++num_ops;
+        }
+        def2num_ops[def] = num_ops;
+    }
+
+    std::queue<Def> queue;
+
+    auto enqueue_uses = [&] (Def def) {
+        for (auto use : def2uses[def]) {
+            if (--def2num_ops[use] == 0)
+                queue.push(use);
+        }
+    };
+
+    const auto& cfg = scope.f_cfg();
 
     for (auto n : cfg.reverse_post_order())
         enqueue_uses(n->lambda());
 
     for (auto n : cfg.reverse_post_order()) {
-        for (auto param : n->lambda()->params()) {
-            if (!param->is_proxy())
-                queue.push(param);
-        }
+        for (auto param : n->lambda()->params())
+            enqueue_uses(param);
 
         while (!queue.empty()) {
             auto def = pop(queue);

42eea3fcaa0fc22189ec38047d3802b6d53a895d
Roland Leißa <leissa@cs.uni-saarland.de>
2016-04-26 19:14:55 +0200

    make free_params aware of MemTypes and FrameTypes

--- src/thorin/analyses/free_params.cpp
+++ src/thorin/analyses/free_params.cpp
@@ -5,26 +5,34 @@
 namespace thorin {
 
 DefSet free_params(const Scope& scope) {
-    DefSet result;
-    DefSet done;
+    DefSet result, done;
     std::queue<const Def*> queue;
 
-    auto enqueue = [&] (const Def* def) {
-        auto p = done.emplace(def);
-        if (p.second)
-            queue.push(def);
+    auto enqueue_ops = [&] (const Def* def) {
+        for (auto op : def->ops()) {
+            auto p = done.emplace(op);
+            if (p.second)
+                queue.push(op);
+        }
     };
 
     for (auto continuation : scope)
-        enqueue(continuation);
+        enqueue_ops(continuation);
 
     while (!queue.empty()) {
         auto def = pop(queue);
         if (auto primop = def->isa<PrimOp>()) {
-            for (auto op : primop->ops())
-                enqueue(op);
+            for (auto op : primop->ops()) {
+                if ((op->type()->isa<MemType>() || op->type()->isa<FrameType>()) && !scope.contains(op)) {
+                    result.emplace(primop);
+                    goto queue_next;
+                }
+            }
+
+            enqueue_ops(primop);
         } else if (!scope.contains(def)) // must be param or primop
             result.emplace(def);
+queue_next:;
     }
 
     return result;

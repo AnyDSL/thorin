045602afdd4485f0fc20376eaebaa111fe683820
Roland Leißa <leissa@cs.uni-saarland.de>
2016-04-26 20:39:26 +0200

    added concat function to merge two containers into one Array

--- src/thorin/util/array.h
+++ src/thorin/util/array.h
@@ -206,6 +206,13 @@ Array<T> ArrayRef<T>::cut(ArrayRef<size_t> indices, size_t reserve) const {
     return result;
 }
 
+template<class T, class U>
+auto concat(const T& a, const U& b) -> Array<typename T::value_type> {
+    Array<typename T::value_type> result(a.size(), b.size());
+    std::copy(b.begin(), b.end(), std::copy(a.begin(), a.end(), result.begin()));
+    return result;
+}
+
 //------------------------------------------------------------------------------
 
 template<class T>

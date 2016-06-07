4c78845a803819f2824b13bc382c021dd1fc05ae
Roland Leißa <leissa@cs.uni-saarland.de>
2016-06-07 14:51:55 +0200

    removed depcrecated free_vars analysis

--- src/thorin/transform/lift_builtins.cpp
+++ src/thorin/transform/lift_builtins.cpp
@@ -1,6 +1,6 @@
 #include "thorin/world.h"
 #include "thorin/analyses/domtree.h"
-#include "thorin/analyses/free_vars.h"
+#include "thorin/analyses/free_params.h"
 #include "thorin/analyses/scope.h"
 #include "thorin/transform/mangle.h"
 
@@ -18,12 +18,13 @@ void lift_builtins(World& world) {
 
     for (auto cur : todo) {
         Scope scope(cur);
-        auto vars = free_vars(scope);
+        auto param_set = free_params(scope);
+        Array<const Def*> params(param_set.begin(), param_set.end());
 #ifndef NDEBUG
-        for (auto var : vars)
+        for (auto var : params)
             assert(var->order() == 0 && "creating a higher-order function");
 #endif
-        auto lifted = lift(scope, {}, vars);
+        auto lifted = lift(scope, {}, params);
 
         std::vector<Use> uses(cur->uses().begin(), cur->uses().end()); // TODO rewrite this
         for (auto use : uses) {
@@ -31,8 +32,8 @@ void lift_builtins(World& world) {
                 if (auto callee = ucontinuation->callee()->isa_continuation()) {
                     if (callee->is_intrinsic()) {
                         auto oops = ucontinuation->ops();
-                        Array<const Def*> nops(oops.size() + vars.size());
-                        std::copy(vars.begin(), vars.end(), std::copy(oops.begin(), oops.end(), nops.begin())); // old ops + former free vars
+                        Array<const Def*> nops(oops.size() + params.size());
+                        std::copy(params.begin(), params.end(), std::copy(oops.begin(), oops.end(), nops.begin())); // old ops + former free params
                         assert(oops[use.index()] == cur);
                         nops[use.index()] = world.global(lifted, lifted->loc(), false, lifted->name);           // update to new lifted continuation
                         ucontinuation->jump(cur, ucontinuation->type_args(), nops.skip_front(), ucontinuation->jump_loc());       // set new args
@@ -45,7 +46,7 @@ void lift_builtins(World& world) {
             }
         }
 
-        assert(free_vars(Scope(lifted)).empty());
+        assert(free_params(Scope(lifted)).empty());
     }
 }
 

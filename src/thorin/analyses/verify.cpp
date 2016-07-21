bc7c2e1b6a0e26eeaef940d4ec2c55b4870458bc
Roland Leißa <leissa@cs.uni-saarland.de>
2016-07-21 18:00:32 +0200

    working on verify

--- src/thorin/analyses/verify.cpp
+++ src/thorin/analyses/verify.cpp
@@ -1,3 +1,5 @@
+#include <stack>
+
 #include "thorin/primop.h"
 #include "thorin/type.h"
 #include "thorin/world.h"
@@ -12,36 +14,130 @@ static void verify_calls(World& world) {
     }
 }
 
-enum Color {
-    White, Gray, Black
+#if 0
+static void detect_cycles(World& world) {
+    DefMap<Color> def2color;
+    std::stack<const Def*> stack;
+
+    for (auto continuation : world.continuations()) {
+        for (auto param : continuation->params())
+            def2color[param] = Black;
+        for (auto op : continuation->ops()) {
+            if (op->isa_continuation())
+                continue;
+
+            if (def2color.emplace(op, White).second) {
+                stack.push(op);
+
+                while (!stack.empty()) {
+                    bool todo = false;
+                    auto def = stack.top();
+                    assert(def->isa<PrimOp>() || def->isa<Param>());
+
+                    assert(def2color.contains(def));
+                    auto& color = def2color.find(def)->second;
+
+                    if (color == White) {
+                        if (auto param = def->isa<Param>()) {
+                            def = param->continuation();
+                            for (auto p : param->continuation()->params())
+                                def2color[p] = Black;
+                        }
+
+                        for (auto op : def->ops()) {
+                            if (op->isa_continuation())
+                                continue;
+
+                            if (def2color.emplace(op, White).second) {
+                                stack.push(op);
+                                todo = true;
+                                continue;
+                            }
+
+                            auto& color = def2color.find(op)->second;
+                            if (color == White) {
+                            } else if (color == Gray) {
+                                ELOG("detected cycle at %", op);
+                            } else { // color == Black
+                            }
+                        }
+                    }
+
+                    if (todo)
+                        color = Gray;
+                    else {
+                        color = Black;
+                        stack.pop();
+                    }
+                }
+            }
+        }
+    }
+}
+#endif
+
+class Cycles {
+public:
+    enum Color {
+        Gray, Black
+    };
+
+    Cycles(World& world)
+        : world_(world)
+    {}
+
+    World& world() { return world_; }
+    void run();
+    void analyze_call(const Continuation*);
+    void analyze(const Def*);
+
+private:
+    World& world_;
+    DefMap<Color> def2color_;
 };
 
-static void visit(PrimOpMap<Color>& primop2color, const PrimOp* primop) {
-    auto& color = primop2color.find(primop)->second;
-    if (color == White) {
-        color = Gray;
-        for (auto op : primop->ops()) {
-            if (auto primop = op->isa<PrimOp>())
-                visit(primop2color, primop);
-        }
-        color = Black;
-    } else if (color == Gray)
-        ELOG("detected primop cycle at %:", primop);
+void Cycles::run() {
+    for (auto continuation : world().continuations())
+        analyze_call(continuation);
 }
 
-static void verify_primops(World& world) {
-    PrimOpMap<Color> primop2color;
+void Cycles::analyze_call(const Continuation* continuation) {
+    for (auto param : continuation->params()) {
+        def2color_[param] = Black; // TODO too relaxed?
+    }
 
-    for (auto primop : world.primops())
-        primop2color[primop] = White;
+    for (auto op : continuation->ops())
+        analyze(op);
+}
 
-    for (auto primop : world.primops())
-        visit(primop2color, primop);
+void Cycles::analyze(const Def* def) {
+    if (def->isa<Continuation>())
+        return;
+
+    auto p = def2color_.emplace(def, Gray);
+    auto& color = p.first->second;
+
+    if (p.second) {
+        if (auto primop = def->isa<PrimOp>()) {
+            for (auto op : primop->ops())
+                analyze(op);
+        } else if (auto param = def->isa<Param>()) {
+            analyze_call(param->continuation());
+        } else if (def->isa<Continuation>())
+            return;
+    } else {
+        if (def->isa<Param>() && color == Gray)
+            WLOG("detected cycle at %", def);
+        // Black: do nothing
+    }
+
+    color = Black;
 }
 
 void verify(World& world) {
     verify_calls(world);
-    verify_primops(world);
+    Cycles cycles(world);
+    cycles.run();
 }
 
 }

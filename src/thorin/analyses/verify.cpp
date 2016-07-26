79a09bf0f4d020d7ec8369a55b000c3fa1a39285
Roland Leißa <leissa@cs.uni-saarland.de>
2016-07-26 19:18:28 +0200

    added TODO

--- src/thorin/analyses/verify.cpp
+++ src/thorin/analyses/verify.cpp
@@ -12,6 +12,7 @@ static void verify_calls(World& world) {
         if (!continuation->empty()) {
             auto callee_fn_type = continuation->callee_fn_type();
             auto arg_fn_type = continuation->arg_fn_type();
+            // TODO check type for equality - this is currently elided due to polymorphism
             if (callee_fn_type->num_args() != arg_fn_type->num_args()) {
                 ELOG("continuation % calls callee % with % arguments but callee expects % arguments", 
                         continuation, continuation->callee(), callee_fn_type->num_args(), arg_fn_type->num_args());

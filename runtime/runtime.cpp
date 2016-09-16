a8e72326cf77ed26f5b30ed892cc65334a68354c
Richard Membarth <richard.membarth@uni-saarland.de>
2016-09-16 12:09:21 +0200

    Define __has_feature for gcc et al.

--- runtime/runtime.cpp
+++ runtime/runtime.cpp
@@ -179,6 +179,9 @@ void thorin_print_float(float f)    { std::cout << f; }
 void thorin_print_double(double d)  { std::cout << d; }
 void thorin_print_string(char* s)   { std::cout << s; }
 
+#ifndef __has_feature
+#define __has_feature(x) 0
+#endif
 #if (defined (__clang__) && !__has_feature(cxx_thread_local)) || defined(_MSC_VER)
 #pragma message("Runtime random function is not thread-safe")
 static std::mt19937 std_gen;

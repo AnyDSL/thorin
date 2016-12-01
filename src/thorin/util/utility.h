60f65e45b216d0457e601a69cc5521e2b00f2241
Stefan Lemme <stefan.lemme@dfki.de>
2016-12-01 15:25:37 +0100

    workaround C3249 for MSVC (illegal constexpr, increment, decrement)

--- src/thorin/util/utility.h
+++ src/thorin/util/utility.h
@@ -119,7 +119,7 @@ constexpr size_t is_power_of_2(size_t i) { return ((i != 0) && !(i & (i - 1))); 
 
 constexpr unsigned log2(unsigned n, unsigned p = 0) { return (n <= 1) ? p : log2(n / 2, p + 1); }
 
-constexpr uint32_t round_to_power_of_2(uint32_t i) {
+inline uint32_t round_to_power_of_2(uint32_t i) {
     i--;
     i |= i >> 1;
     i |= i >> 2;

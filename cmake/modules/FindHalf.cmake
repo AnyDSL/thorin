581e34aa5b0492ca7b8b1c4c1d73212951853f93
Richard Membarth <richard.membarth@uni-saarland.de>
2017-02-21 18:26:25 +0100

    Half: add include dir to search hints.

--- cmake/modules/FindHalf.cmake
+++ cmake/modules/FindHalf.cmake
@@ -1,18 +1,18 @@
 # Find the Half library
 #
 # To set manually the paths, define these environment variables:
-#  Half_DIR       - Include path
+#  Half_DIR           - Include path
 #
 # Once done this will define
 #  Half_INCLUDE_DIRS  - where to find Half library include file
 #  Half_FOUND         - True if Half library is found
 
-SET(Half_DIR $ENV{Half_DIR} CACHE PATH "C++ library for half precision floating point arithmetics.")
-FIND_PATH(Half_INCLUDE_DIR half.hpp PATHS ${Half_DIR})
+set(Half_DIR $ENV{Half_DIR} CACHE PATH "C++ library for half precision floating point arithmetics.")
+find_path(Half_INCLUDE_DIR half.hpp PATHS ${Half_DIR} ${Half_DIR}/include)
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Half DEFAULT_MSG Half_INCLUDE_DIR)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Half DEFAULT_MSG Half_INCLUDE_DIR)
 
-SET(Half_INCLUDE_DIRS ${Half_INCLUDE_DIR})
+set(Half_INCLUDE_DIRS ${Half_INCLUDE_DIR})
 
-MARK_AS_ADVANCED(Half_INCLUDE_DIR)
+mark_as_advanced(Half_INCLUDE_DIR)

f12abe3ad5f3398a53d3ca6fe4a27d7a9eb2a1a6
Stefan Lemme <stefan.lemme@dfki.de>
2017-03-16 14:38:35 +0100

    find_path() of Half_INCLUDE_DIR may use predefined ${Half_INCLUDE_DIR}

--- cmake/modules/FindHalf.cmake
+++ cmake/modules/FindHalf.cmake
@@ -8,7 +8,7 @@
 #  Half_FOUND         - True if Half library is found
 
 set(Half_DIR $ENV{Half_DIR} CACHE PATH "C++ library for half precision floating point arithmetics.")
-find_path(Half_INCLUDE_DIR half.hpp PATHS ${Half_DIR} ${Half_DIR}/include)
+find_path(Half_INCLUDE_DIR half.hpp PATHS ${Half_DIR} ${Half_INCLUDE_DIR} PATH_SUFFIXES include)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Half DEFAULT_MSG Half_INCLUDE_DIR)

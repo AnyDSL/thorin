67afcce4e12e6e1c38e3659a4b9f58da1ef41dd0
Roland Leißa <leissa@cs.uni-saarland.de>
2017-03-16 02:42:51 +0100

    fine-tune inliner + bug fix

--- src/thorin/transform/inliner.cpp
+++ src/thorin/transform/inliner.cpp
@@ -38,12 +38,18 @@ void force_inline(Scope& scope, int threshold) {
 void inliner(World& world) {
     static const int factor = 4;
     static const int offset = 4;
-    Scope::for_each(world, [] (const Scope& scope) {
-        if (scope.defs().size() < scope.entry()->num_params() * factor + offset)
-            for (const auto& use : scope.entry()->copy_uses())
-                if (auto ucontinuation = use->isa_continuation())
-                    if (use.index() == 0)
+    Scope::for_each(world, [] (Scope& scope) {
+        if (scope.defs().size() < scope.entry()->num_params() * factor + offset) {
+            for (const auto& use : scope.entry()->copy_uses()) {
+                if (auto ucontinuation = use->isa_continuation()) {
+                    if (use.index() == 0) {
                         ucontinuation->jump(drop(scope, ucontinuation->args()), {}, ucontinuation->jump_debug());
+                        if (scope.contains(ucontinuation))
+                            scope.update();
+                    }
+                }
+            }
+        }
     });
 
     debug_verify(world);

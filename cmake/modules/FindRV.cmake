38213fb36337ffa45ac221e2c9bb9d7ef6181f34
Richard Membarth <richard.membarth@uni-saarland.de>
2017-05-04 20:50:21 +0200

    RV is now built as part of LLVM.

---
+++ cmake/modules/FindRV.cmake
@@ -0,0 +1,17 @@
+# Find the RV library
+#
+# Once done this will define
+#  RV_INCLUDE_DIRS  - where to find RV library include file
+#  RV_LIBRARIES     - where to find RV library
+#  RV_FOUND         - True if RV library is found
+
+find_path(RV_INCLUDE_DIR rv.h PATHS ${LLVM_INCLUDE_DIRS} PATH_SUFFIXES rv)
+find_library(RV_LIBRARY  RV   PATHS ${LLVM_LIBRARY_DIRS})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(RV DEFAULT_MSG RV_INCLUDE_DIR RV_LIBRARY)
+
+set(RV_INCLUDE_DIRS ${RV_INCLUDE_DIR})
+set(RV_LIBRARIES    ${RV_LIBRARY})
+
+mark_as_advanced(RV_INCLUDE_DIR RV_LIBRARY)

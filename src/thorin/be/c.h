c60428ccdbc91505e06b8dfb4c3d74a483b840d6
Roland Leißa <leissa@cs.uni-saarland.de>
2017-09-06 15:30:17 +0200

    don't include llvm in c backend

--- src/thorin/be/c.h
+++ src/thorin/be/c.h
@@ -4,11 +4,12 @@
 #include <cstdint>
 #include <iostream>
 
-#include "thorin/be/llvm/llvm.h"
+#include "thorin/continuation.h"
 
 namespace thorin {
 
 class World;
+typedef ContinuationMap<std::tuple<int, int, int>> Cont2Config;
 
 enum class Lang : uint8_t {
     C99,    ///< Flag for C99

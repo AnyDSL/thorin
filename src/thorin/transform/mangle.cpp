83c517d93f0372f44559d0258fa2ad14df9dd52d
Roland Leißa <leissa@cs.uni-saarland.de>
2017-09-07 23:54:21 +0200

    trick in mangle to eat up "returns"

--- src/thorin/transform/mangle.cpp
+++ src/thorin/transform/mangle.cpp
@@ -95,6 +95,39 @@ Continuation* Mangler::mangle() {
     }
 
     mangle_body(old_entry(), new_entry());
+
+    if (!lift_.empty())
+        return new_entry();
+
+    // this is the eat-up-return trick
+    // if the new continuations only contains one call to one of its arguments - eat up this call, too
+    ContinuationSet rets;
+    for (auto arg : args_) {
+        if (arg && arg->isa_continuation())
+            rets.emplace(arg->as_continuation());
+    }
+
+    if (rets.empty())
+        return new_entry();
+
+    Continuation* fold = nullptr;
+    for (auto new_continuation : new_continuations_) {
+        if (auto callee = new_continuation->callee()->isa_continuation()) {
+            if (rets.contains(callee)) {
+                if (fold == nullptr)
+                    fold = new_continuation;
+                else
+                    return new_entry(); // more then one "returns"
+            }
+        }
+    }
+
+    if (fold != nullptr) {
+        Scope s(fold->callee()->as_continuation());
+        auto dropped = drop(s, fold->args());
+        fold->jump(dropped, {}, fold->jump_debug());
+    }
+
     return new_entry();
 }
 
@@ -112,6 +145,7 @@ Continuation* Mangler::mangle_head(Continuation* old_continuation) {
 
 void Mangler::mangle_body(Continuation* old_continuation, Continuation* new_continuation) {
     assert(!old_continuation->empty());
+    new_continuations_.emplace_back(new_continuation);
 
     if (old_continuation->callee() == world().branch()) {        // fold branch if possible
         if (auto lit = mangle(old_continuation->arg(0))->isa<PrimLit>())
@@ -151,7 +185,6 @@ const Def* Mangler::mangle(const Def* old_def) {
     else if (!within(old_def))
         return old_def;
     else if (auto old_continuation = old_def->isa_continuation()) {
-
         auto new_continuation = mangle_head(old_continuation);
         mangle_body(old_continuation, new_continuation);
         return new_continuation;

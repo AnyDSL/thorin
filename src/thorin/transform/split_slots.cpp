8ffa5f1489a6cbfb3ef4fdf514e039969cf20b64
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2017-09-07 18:24:37 +0200

    Improve alloca splitting

--- src/thorin/transform/split_slots.cpp
+++ src/thorin/transform/split_slots.cpp
@@ -8,25 +8,64 @@
 
 namespace thorin {
 
+struct IndexHash {
+    static uint64_t hash(u32 u) { return u; }
+    static bool eq(u32 a, u32 b) { return a == b; }
+    static u32 sentinel() { return 0xFFFFFFFF; }
+};
+
 static void split(const Slot* slot) {
-    Def2Def new_slots;
+    auto array_type = slot->alloced_type()->as<DefiniteArrayType>();
+    auto dim = array_type->dim();
+    auto elem_type = array_type->elem_type();
+
+    HashMap<u32, const Def*, IndexHash> new_slots;
     auto& world = slot->world();
 
-    for (auto use : slot->uses()) {
-        auto lea = use->as<LEA>();
-        auto index = lea->index();
+    auto elem_slot = [&] (u32 index) {
         if (!new_slots.contains(index))
-            new_slots[index] = world.slot(lea->type()->as<PtrType>()->pointee(), slot->frame(), slot->debug());
-        lea->replace(new_slots[index]);
+            new_slots[index] = world.slot(elem_type, slot->frame(), slot->debug());
+        return new_slots[index];
+    };
+
+    for (auto use : slot->copy_uses()) {
+        if (auto lea = use->isa<LEA>()) {
+            lea->replace(elem_slot(lea->index()->as<PrimLit>()->value().get_u32()));
+        } else if (auto store = use->isa<Store>()) {
+            auto in_mem = store->op(0);
+            for (size_t i = 0, e = dim; i != e; ++i) {
+                auto elem = world.extract(store->op(1), i, store->debug());
+                in_mem = world.store(in_mem, elem_slot(i), elem, store->debug());
+            }
+            store->replace(in_mem);
+        } else if (auto load = use->isa<Load>()) {
+            auto in_mem = load->op(0);
+            auto array = world.bottom(array_type, load->debug());
+            for (size_t i = 0, e = dim; i != e; ++i) {
+                auto load_tuple = world.load(in_mem, elem_slot(i), load->debug());
+                auto elem = world.extract(load_tuple, 1, load->debug());
+                in_mem = world.extract(load_tuple, 1, load->debug());
+                array = world.insert(array, i, elem, load->debug());
+            }
+            load->replace(world.tuple({ in_mem, array }, load->debug()));
+        }
     }
 }
 
 static bool can_split(const Slot* slot) {
+    if (!slot->alloced_type()->isa<DefiniteArrayType>())
+        return false;
+
+    // only accept LEAs with constant indices and loads and stores
     for (auto use : slot->uses()) {
-        auto lea = use->isa<LEA>();
-        if (!lea || !is_const(lea->index()))
+        if (auto lea = use->isa<LEA>()) {
+            if (!is_const(lea->index()))
+                return false;
+        } else if (!use->isa<Store>() && !use->isa<Load>()) {
             return false;
+        }
     }
+
     return true;
 }
 

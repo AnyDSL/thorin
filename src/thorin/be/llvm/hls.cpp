2cdef1c00f8177582ee1b44a5e145ce537aa135c
Richard Membarth <richard.membarth@uni-saarland.de>
2017-10-30 14:48:28 +0100

    HLS back end files.

---
+++ src/thorin/be/llvm/hls.cpp
@@ -0,0 +1,24 @@
+#include "thorin/be/llvm/hls.h"
+
+#include <fstream>
+#include <stdexcept>
+
+#include "thorin/primop.h"
+#include "thorin/world.h"
+#include "thorin/be/c.h"
+
+namespace thorin {
+
+HLSCodeGen::HLSCodeGen(World& world, const Cont2Config& kernel_config)
+    : CodeGen(world, llvm::CallingConv::C, llvm::CallingConv::C, llvm::CallingConv::C, kernel_config)
+{}
+
+void HLSCodeGen::emit(bool debug) {
+    auto name = get_output_name(world_.name());
+    std::ofstream file(name);
+    if (!file.is_open())
+        throw std::runtime_error("cannot write '" + name + "': " + strerror(errno));
+    thorin::emit_c(world_, kernel_config_, file, Lang::C99, debug);
+}
+
+}
---
+++ src/thorin/be/llvm/hls.h
@@ -0,0 +1,21 @@
+#ifndef THORIN_BE_LLVM_HLS_H
+#define THORIN_BE_LLVM_HLS_H
+
+#include "thorin/be/llvm/llvm.h"
+
+namespace thorin {
+
+class HLSCodeGen : public CodeGen {
+public:
+    HLSCodeGen(World& world, const Cont2Config&);
+
+    void emit(bool debug);
+
+protected:
+    virtual std::string get_alloc_name() const { THORIN_UNREACHABLE; /*alloc not supported in HLS*/; }
+    virtual std::string get_output_name(const std::string& name) const { return name + ".hls"; }
+};
+
+}
+
+#endif

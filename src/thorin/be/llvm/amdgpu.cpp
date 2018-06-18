1c5aac71e7ee67a679633a5788cd398de795bf61
Richard Membarth <richard.membarth@uni-saarland.de>
2018-06-18 18:29:41 +0200

    Update amdgpu target triple.

--- src/thorin/be/llvm/amdgpu.cpp
+++ src/thorin/be/llvm/amdgpu.cpp
@@ -11,7 +11,7 @@ AMDGPUCodeGen::AMDGPUCodeGen(World& world, const Cont2Config& kernel_config)
     , kernel_config_(kernel_config)
 {
     module_->setDataLayout("e-p:32:32-p1:64:64-p2:64:64-p3:32:32-p4:64:64-p5:32:32-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024-v2048:2048-n32:64");
-    module_->setTargetTriple("amdgcn-amd-amdhsa-amdgiz");
+    module_->setTargetTriple("amdgcn-amd-amdhsa");
 }
 
 //------------------------------------------------------------------------------

a1da271a381df35a0db72fd9ccdaa43f37dccc73
Roland Leißa <leissa@cs.uni-saarland.de>
2018-10-19 17:41:11 +0200

    update codegen_prepare for tuple handling

--- src/thorin/transform/codegen_prepare.cpp
+++ src/thorin/transform/codegen_prepare.cpp
@@ -13,11 +13,39 @@ void codegen_prepare(World& world) {
         auto ret_cont = world.continuation(ret_param->type()->as<FnType>(), ret_param->debug());
         ret_cont->jump(ret_param, ret_cont->param(), ret_param->debug());
 
-        // TODO broken with new tuple handling
         for (auto use : ret_param->copy_uses()) {
             if (auto ucontinuation = use->isa_continuation()) {
                 if (use.index() != 0) {
-                    ucontinuation->update_op(use.index(), ret_cont);
+                    assert(use.index() == 1);
+                    ucontinuation->update_arg(ret_cont);
+                    dirty = true;
+                }
+            } else if (auto tuple = use->isa<Tuple>()) {
+                Array<const Def*> ops(tuple->ops());
+                ops[use.index()] = ret_cont;
+                for (auto use : tuple->uses()) {
+                    if (auto ucontinuation = use->isa_continuation()) {
+                        ucontinuation->update_arg(world.tuple(ops));
+                        dirty = true;
+                    }
+                }
+            }
+        }
+
+        for (auto use : scope.entry()->param()->copy_uses()) {
+            if (auto ucontinuation = use->isa_continuation()) {
+                if (use.index() != 0) {
+                    assert(use.index() == 1);
+                    Array<const Def*> args(scope.entry()->num_params());
+                    size_t i = 0;
+                    for (auto param : scope.entry()->params()) {
+                        if (param == ret_param)
+                            args[i++] = ret_cont;
+                        else
+                            args[i++] = param;
+                    }
+
+                    ucontinuation->update_arg(world.tuple(args));
                     dirty = true;
                 }
             }

02b634b0521a8e07be8f03dd7df093ee471075bf
Sebastian Wack <info@sebastianwack.de>
2018-12-12 17:26:06 +0100

    Added special deep_copy buffer case

--- src/thorin/transform/deep_copy.cpp
+++ src/thorin/transform/deep_copy.cpp
@@ -81,7 +81,6 @@ public:
                 auto outputValue = world.extract(outputDef, (qu32) 1);
                 mem = world.extract(outputDef, (qu32) 0);
 
-                std::cerr << pointer_type->to_string() << std::endl;
                 check_continuation_availability(pointee);
 
                 auto pointee_cont = emit(pointee);
@@ -96,6 +95,56 @@ public:
                 if(struct_type) {
                     dimension = struct_type->num_ops();
                     debug_name = "deep_copy_struct_cont";
+                    if(dimension == 3 && struct_type->name().str() == "Buffer" ) {
+                        //special case: Buffer
+                        auto sizePtr = world.lea(input, world.literal_qu32((qu32) 1, {}), {});
+                        auto inputArrayPtrPtr = world.lea(input, world.literal_qu32((qu32) 0, {}), {});
+                        auto outputArrayPtrPtr = world.lea(output, world.literal_qu32((qu32) 0, {}), {});
+                        //load input and output array ptr ptr's
+                        auto inputArrayPtr = world.load(mem, inputArrayPtrPtr);
+                        mem = world.extract(inputArrayPtr, world.literal_qu32((qu32) 0, {}), {});
+                        inputArrayPtr = world.extract(inputArrayPtr, world.literal_qu32((qu32) 1, {}), {});
+                        auto outputArrayPtr = world.load(mem, outputArrayPtrPtr);
+                        mem = world.extract(outputArrayPtr, world.literal_qu32((qu32) 0, {}), {});
+                        outputArrayPtr = world.extract(outputArrayPtr, world.literal_qu32((qu32) 1, {}), {});
+                        //load size and cast it to qs32
+                        auto size = world.load(mem, sizePtr);
+                        mem = world.extract(size, world.literal_qu32((qu32) 0, {}), {});
+                        size = world.extract(size, world.literal_qu32((qu32) 1, {}), {});
+                        size = world.cast(world.type_qs32(1), size, {});
+
+                        auto loop = world.continuation(world.fn_type({world.mem_type(), world.type_qs32(1)}),Debug(Symbol("loop")));
+                        cont->jump(loop, {mem, world.literal_qs32((qs32) 0, {})});
+
+                        auto loop_mem = loop->param(0);
+                        auto loop_index = loop->param(1);
+
+                        //generate continuations of loop branch
+                        auto if_then = world.continuation(world.fn_type(), Debug(Symbol("if_then")));
+                        auto if_else = world.continuation(world.fn_type(), Debug(Symbol("if_else")));
+
+                        //generate condition of loop
+                        auto condition = world.cmp_lt(loop_index, size);
+                        loop->branch(condition, if_then, if_else);
+
+                        //fill branch continuations
+                        //load input
+                        auto inputPtr = if_then->world().lea(inputArrayPtr, loop_index, {});
+                        auto input_value = if_then->world().load(loop_mem, inputPtr);
+                        auto if_then_mem = if_then->world().extract(input_value, world.literal_qu32((qu32) 0, {}), {});
+                        input_value = if_then->world().extract(input_value, world.literal_qu32((qu32) 1, {}), {});
+                        //load output pointer
+                        auto outputPtr = if_then->world().lea(outputArrayPtr, loop_index, {});
+                        //store loaded value
+                        if_then_mem = if_then->world().store(if_then_mem, outputPtr, input_value, {});
+                        //increse loop counter
+                        auto new_loop_index = world.arithop_add(world.literal_qs32((qs32) 1, {}), loop_index,{});
+                        if_then->jump(loop, {if_then_mem, new_loop_index});
+
+                        if_else->jump(return_fn, {loop_mem});
+
+                        return cont;
+                    }
                 }
                 else {
                     dimension = tuple_type->num_ops();

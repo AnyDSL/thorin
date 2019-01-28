4ac3a29a557ce2ed63ad9b05f0f4a0fe0698521b
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2019-01-28 12:21:06 +0100

    Fix codegen_prepare

--- src/thorin/transform/codegen_prepare.cpp
+++ src/thorin/transform/codegen_prepare.cpp
@@ -4,58 +4,46 @@
 
 namespace thorin {
 
+static bool replace_param(const Def* param, const Def* replace_with) {
+    bool dirty = false;
+    for (auto use : param->copy_uses()) {
+        // Check if the use is a call
+        if (auto app = use->isa<App>()) {
+            // For all uses of the call
+            for (auto app_use : app->copy_uses()) {
+                if (auto lam = app_use->isa_lam()) {
+                    if (lam == replace_with)
+                        continue;
+                    dirty = true;
+                    if (use.index() == 0) {
+                        // Replace by a call to the new callee, if it is not the new callee itself
+                        lam->app(replace_with, app->op(1));
+                    } else {
+                        assert(use.index() == 1);
+                        lam->app(app->op(0), replace_with);
+                    }
+                }
+            }
+        } else if (auto tuple = use->isa<Tuple>()) {
+            auto& world = tuple->world();
+            Array<const Def*> args(tuple->num_ops());
+            for (size_t i = 0, n = tuple->num_ops(); i < n; ++i)
+                args[i] = tuple->op(i);
+            args[use.index()] = replace_with;
+            dirty |= replace_param(tuple, world.tuple(args));
+        }
+    }
+    return dirty;
+}
+
 void codegen_prepare(World& world) {
     VLOG("start codegen_prepare");
     Scope::for_each(world, [&](Scope& scope) {
         DLOG("scope: {}", scope.entry());
-        bool dirty = false;
         auto ret_param = scope.entry()->ret_param();
         auto ret_cont = world.lam(ret_param->type()->as<Pi>(), ret_param->debug());
         ret_cont->app(ret_param, ret_cont->param(), ret_param->debug());
-
-        for (auto use : ret_param->copy_uses()) {
-            if (auto ulam = use->isa_lam()) {
-                if (use.index() != 0) {
-                    assert(use.index() == 1);
-                    ulam->set_body(ret_cont);
-                    dirty = true;
-                }
-            } else if (auto tuple = use->isa<Tuple>()) {
-                Array<const Def*> ops(tuple->ops());
-                ops[use.index()] = ret_cont;
-                for (auto use : tuple->uses()) {
-                    if (auto ulam = use->isa_lam()) {
-                        if (auto app = ulam->app()) {
-                            ulam->app(app->callee(), ops, app->debug());
-                            dirty = true;
-                        }
-                    }
-                }
-            }
-        }
-
-        for (auto use : scope.entry()->param()->copy_uses()) {
-            if (auto ulam = use->isa_lam()) {
-                if (use.index() != 0) {
-                    assert(use.index() == 1);
-                    Array<const Def*> args(scope.entry()->num_params());
-                    size_t i = 0;
-                    for (auto param : scope.entry()->params()) {
-                        if (param == ret_param)
-                            args[i++] = ret_cont;
-                        else
-                            args[i++] = param;
-                    }
-
-                    if (auto app = ulam->app()) {
-                        ulam->app(app->callee(), args, app->debug());
-                        dirty = true;
-                    }
-                }
-            }
-        }
-
-        if (dirty)
+        if (replace_param(ret_param, ret_cont))
             scope.update();
     });
     VLOG("end codegen_prepare");

f57649da7d4a6922ea7b87deca9320223d690d15
Roland Leißa <leissa@cs.uni-saarland.de>
2019-03-29 00:16:30 +0100

    util: bug fix for merge

--- src/thorin/util.cpp
+++ src/thorin/util.cpp
@@ -133,22 +133,26 @@ Array<const Def*> merge(const Def* def, Defs defs) {
     return Array<const Def*>(defs.size() + 1, [&](auto i) { return i == 0 ? def : defs[i-1]; });
 }
 
-Array<const Def*> merge_tuple_or_sigma(const Def* tuple_or_sigma, Defs defs) {
-    Array<const Def*> result(tuple_or_sigma->num_ops() + defs.size());
-    auto i = std::copy(tuple_or_sigma->ops().begin(), tuple_or_sigma->ops().end(), result.begin());
-    std::copy(defs.begin(), defs.end(), i);
+Array<const Def*> merge(Defs a, Defs b) {
+    Array<const Def*> result(a.size() + b.size());
+    auto i = std::copy(a.begin(), a.end(), result.begin());
+    std::copy(b.begin(), b.end(), i);
     return result;
 }
 
 const Def* merge_sigma(const Def* def, Defs defs) {
     if (auto sigma = def->isa<Sigma>(); sigma && !sigma->isa_nominal())
-        return def->world().sigma(merge_tuple_or_sigma(sigma, defs));
+        return def->world().sigma(merge(sigma->ops(), defs));
     return def->world().sigma(merge(def, defs));
 }
 
 const Def* merge_tuple(const Def* def, Defs defs) {
-    if (auto tuple = def->isa<Tuple>(); tuple && !tuple->type()->isa_nominal())
-        return def->world().tuple(merge_tuple_or_sigma(tuple, defs));
+    auto& w = def->world();
+    if (auto sigma = def->type()->isa<Sigma>(); sigma && !sigma->isa_nominal()) {
+        Array<const Def*> tuple(sigma->num_ops(), [&](auto i) { return w.extract(def, i); });
+        return w.tuple(merge(tuple, defs));
+    }
+
     return def->world().tuple(merge(def, defs));
 }
 

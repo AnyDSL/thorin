22d8e116fd625263c9de3683f1a37c60d319a73f
Roland Leißa <leissa@cs.uni-saarland.de>
2019-06-27 17:06:25 +0200

    removed deprecated passes

--- src/thorin/pass/optimize.cpp
+++ src/thorin/pass/optimize.cpp
@@ -5,11 +5,8 @@
 
 // old stuff
 #include "thorin/transform/cleanup_world.h"
-#include "thorin/transform/clone_bodies.h"
 #include "thorin/transform/codegen_prepare.h"
 #include "thorin/transform/flatten_tuples.h"
-#include "thorin/transform/inliner.h"
-#include "thorin/transform/lift_builtins.h"
 #include "thorin/transform/partial_evaluation.h"
 
 namespace thorin {
@@ -28,9 +25,6 @@ void optimize_old(World& world) {
     cleanup_world(world);
     while (partial_evaluation(world, true)); // lower2cff
     flatten_tuples(world);
-    clone_bodies(world);
-    lift_builtins(world);
-    inliner(world);
     cleanup_world(world);
     codegen_prepare(world);
 }

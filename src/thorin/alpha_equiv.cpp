29586d4d9ac5b44ad733145c8b8fd27bb9541c86
Roland Leißa <leissa@cs.uni-saarland.de>
2019-09-17 14:43:13 +0200

    polish

--- src/thorin/alpha_equiv.cpp
+++ src/thorin/alpha_equiv.cpp
@@ -7,37 +7,28 @@ struct Checker {
 
     bool run(const Def* d1, const Def* d2) {
         if (d1 == d2 || (!d1->is_set() && !d2->is_set())) return true;
-        if (d1->node() != d2->node() || d1->fields() != d2->fields() || d1->num_ops() != d2->num_ops()) return false;
+        if (d1->node() != d2->node() || d1->fields() != d2->fields() || d1->num_ops() != d2->num_ops()
+                || bool(d1->isa_nominal()) != bool(d2->isa_nominal())) return false;
         if (d1->gid() > d2->gid()) std::swap(d1, d2); // normalize: always put smaller gid to the left
-        if (equiv.contains({d1, d2})) return true;
 
-        bool result = true;
+        // this assumption will either be true - or we will bail out with false anyway
+        auto [i, success] = equiv.emplace(d1, d2);
+        if (!success) return true;
+
+        // params are equal if they appeared under the same binder
         if (auto p1 = d1->isa<Param>()) {
-            auto p2 = d2->as<Param>();
             for (auto [q1, q2] : params) {
-                if (p1 == q1) return p2 == q2;
+                if (p1 == q1) return d2->as<Param>() == q2;
             }
-        } else if (auto n1 = d1->isa_nominal()) {
-            if (auto n2 = d2->isa_nominal()) {
-                params.emplace_back(n1->param(), n2->param());
-                equiv.emplace(n1, n2);
-                for (size_t i = 0, e = n1->num_ops(); i != e && result; ++i)
-                    result &= run(n1->op(i), n2->op(i));
-            } else {
-                return false;
-            }
-        } else {
-            if (!d2->isa_nominal()) {
-                for (size_t i = 0, e = d1->num_ops(); i != e && result; ++i)
-                    result &= run(d1->op(i), d2->op(i));
-
-                if (result)
-                    equiv.emplace(d1, d2);
-            } else {
-                return false;
-            }
+            return true;
         }
 
+        if (auto n1 = d1->isa_nominal())
+            params.emplace_back(n1->param(), d2->as_nominal()->param());
+
+        bool result = true;
+        for (size_t i = 0, e = d1->num_ops(); i != e && result; ++i)
+            result &= run(d1->op(i), d2->op(i));
         return result;
     }
 

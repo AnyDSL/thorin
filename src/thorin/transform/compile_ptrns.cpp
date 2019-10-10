0e282046efa78f9ff67d07d570d111617a909a09
Arsène Pérard-Gayot <perard@cg.uni-saarland.de>
2019-10-10 18:34:01 +0200

    Working compilation

--- src/thorin/transform/compile_ptrns.cpp
+++ src/thorin/transform/compile_ptrns.cpp
@@ -12,83 +12,103 @@ class PtrnCompiler {
 private:
     /// Flattens the tuples/packs in a pattern
     Ptrn* flatten(Ptrn* ptrn) {
-        auto& world = ptrn->world();
         Scope scope(ptrn);
-        Rewriter rewriter(world, &scope);
-        auto expanded = world.ptrn(world.case_(flattener.flatten(ptrn->type()->domain()), ptrn->type()->codomain()), ptrn->debug());
-        rewriter.old2new.emplace(ptrn->param(), unflatten(expanded->param(), ptrn->type()->domain()));
-        expanded->set(flattener.flatten(rewriter.rewrite(ptrn->matcher())), rewriter.rewrite(ptrn->body()));
-        return expanded;
+        Rewriter rewriter(world_, &scope);
+        auto f_ptrn = world_.ptrn(world_.case_(flattener_.flatten(ptrn->type()->domain()), ptrn->type()->codomain()), ptrn->debug());
+        rewriter.old2new.emplace(ptrn->param(), unflatten(f_ptrn->param(), ptrn->type()->domain()));
+        f_ptrn->set(flattener_.flatten(rewriter.rewrite(ptrn->matcher())), rewriter.rewrite(ptrn->body()));
+        return f_ptrn;
     }
 
-    /// Eliminates element 'col' from @p def
-    static const Def* eliminate(const Def* def, size_t col) {
-        auto& world = def->world();
-        if (auto tuple = def->isa<Tuple>()) {
-            Array<const Def*> ops(tuple->num_ops() - 1);
-            auto cur = std::copy(tuple->ops().begin(), tuple->ops().begin() + col, ops.begin());
-            std::copy(tuple->ops().begin() + col + 1, tuple->ops().end(), cur);
-            return world.tuple(ops, tuple->debug());
-        } else if (auto pack = def->isa<Pack>()) {
-            assert(pack->type()->arity()->isa<Lit>());
-            return world.pack(pack->type()->lit_arity() - 1, pack->body(), pack->debug());
-        } else {
-            THORIN_UNREACHABLE;
-        }
+    /// Eliminates an element from a def
+    const Def* eliminate(const Def* def, size_t col) {
+        Array<const Def*> ops(def->type()->lit_arity() - 1);
+        for (size_t i = 0; i < col; ++i)
+            ops[i] = def->out(i);
+        for (size_t i = col, n = ops.size(); i < n; ++i)
+            ops[i] = def->out(i + 1);
+        return world_.tuple(ops);
     }
 
-    Ptrn* specialize(Ptrn* ptrn, size_t col, const Def* val = nullptr) {
-        // TODO
-        return nullptr;
+    /// Introduces an element in a def
+    const Def* introduce(const Def* def, size_t col, const Def* val) {
+        Array<const Def*> ops(def->type()->lit_arity() + 1);
+        for (size_t i = 0; i < col; ++i)
+            ops[i] = def->out(i);
+        ops[col] = val;
+        for (size_t i = col + 1, n = ops.size(); i < n; ++i)
+            ops[i] = def->out(i - 1);
+        return world_.tuple(ops);
     }
 
-    Flattener flattener;
+    /// Specializes the pattern for one particular value
+    Ptrn* specialize(Ptrn* ptrn, size_t col, const Def* val, const Def* s_type) {
+        Scope scope(ptrn);
+        Rewriter rewriter(world_, &scope);
+        auto s_ptrn = world_.ptrn(world_.case_(s_type, ptrn->body()->type()), ptrn->debug());
+        rewriter.old2new.emplace(ptrn->param(), introduce(s_ptrn->param(), col, val));
+        s_ptrn->set(eliminate(rewriter.rewrite(ptrn->matcher()), col), rewriter.rewrite(ptrn->body()));
+        return s_ptrn;
+    }
+
+    World& world_;
+    Flattener flattener_;
 
 public:
+    PtrnCompiler(World& world)
+        : world_(world)
+    {}
+
     const Def* compile(const Def* arg, std::vector<Ptrn*>& ptrns, const Def* dbg) {
         assert(!ptrns.empty());
+        if (arg->type()->lit_arity() == 1) {
+            // The reinterpret_cast is need to case the Ptrn** into Def**,
+            // which is not a valid C++ static_cast, but should be safe
+            // because we do not modify the contents of the array.
+            return world_.match(arg, Defs(ptrns.size(), reinterpret_cast<Def**>(ptrns.data())), dbg);
+        }
 
         // Flatten tuple patterns
-        arg = flattener.flatten(arg);
+        arg = flattener_.flatten(arg);
         for (auto& ptrn : ptrns)
             ptrn = flatten(ptrn);
 
         // Select a column to specialize on
         size_t col = 0; // TODO: Heuristics
-        auto col_type = proj(arg->type(), col);
+        auto col_arg = arg->out(col);
+        auto col_type = col_arg->type();
 
         // Generate specialized patterns for each constructor pattern
         // and collect patterns that have no constructor.
-        auto& world = arg->world();
         std::vector<Ptrn*> no_ctor;
         DefMap<std::vector<Ptrn*>> ctor2ptrns;
+        auto s_arg = eliminate(arg, col);
         for (auto ptrn : ptrns) {
             auto ptrn_col = ptrn->matcher()->out(col);
             if (auto lit = ptrn_col->isa<Lit>()) {
-                ctor2ptrns[lit].push_back(specialize(ptrn, col, lit));
+                ctor2ptrns[lit].push_back(specialize(ptrn, col, lit, s_arg->type()));
             } else {
-                no_ctor.push_back(specialize(ptrn, col));
+                no_ctor.push_back(specialize(ptrn, col, col_arg, s_arg->type()));
             }
         }
 
         // Generate a new match for each constructor
         std::vector<const Def*> compiled_ptrns;
-        auto s_arg = eliminate(arg, col);
         for (auto& [ctor, ctor_ptrns] : ctor2ptrns) {
             auto value = compile(s_arg, ctor_ptrns, dbg);
-            auto ptrn = world.ptrn(world.case_(col_type, value->type()), dbg);
+            auto ptrn = world_.ptrn(world_.case_(col_type, value->type()), dbg);
             ptrn->set(ctor, value);
             compiled_ptrns.push_back(ptrn);
         }
         // TODO: Completeness/redundancy check
         if (!no_ctor.empty()) {
             auto value = compile(s_arg, no_ctor, dbg);
-            auto ptrn = world.ptrn(world.case_(col_type, value->type()), dbg);
+            auto ptrn = world_.ptrn(world_.case_(col_type, value->type()), dbg);
             ptrn->set(ptrn->param(), value);
             compiled_ptrns.push_back(ptrn);
         }
 
-        return world.match(arg->out(col), compiled_ptrns, dbg);
+        return world_.match(col_arg, compiled_ptrns, dbg);
     }
 
     const Def* compile(const Match* match) {
@@ -100,7 +120,7 @@ public:
 };
 
 void compile_ptrns(World& world) {
-    PtrnCompiler compiler;
+    PtrnCompiler compiler(world);
     world.rewrite("compile_ptrns",
         [&](const Scope& scope) {
             return scope.entry()->isa<Lam>();

daad51155c7a0ed62baa24ee503be23fe0e3cba8
Puya Amiri <puya.amiri@dfki.de>
2020-04-29 19:58:29 +0200

    hls_kernel_launch added to CMAKE

--- src/thorin/transform/hls_kernel_launch.cpp
+++ src/thorin/transform/hls_kernel_launch.cpp
@@ -1,10 +1,13 @@
+#include "thorin/transform/hls_kernel_launch.h"
 #include "thorin/world.h"
 #include "thorin/continuation.h"
-#include "thorin/util/log.h"
 #include "thorin/analyses/scope.h"
 #include "thorin/analyses/schedule.h"
 #include "thorin/util/log.h"
 
 namespace thorin {
 
+    void hls_kernel_launch(World& world) {
+
+    }
 }

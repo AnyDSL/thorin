b80b24c7b08ad8cfe5c8f4345bdf7a315df7c32a
Roland Leißa <leissa@cs.uni-saarland.de>
2020-11-08 04:40:30 +0100

    reworked BetaEtaConv

--- src/thorin/pass/beta_eta_conv.cpp
+++ src/thorin/pass/beta_eta_conv.cpp
@@ -22,55 +22,40 @@ static bool is_free(const Param* param, const Def* def) {
     return is_free(done, param, def);
 }
 
-BetaEtaConv::Lattice BetaEtaConv::lattice(Lam* lam) {
-    if (auto glob_i = lam2glob_.find(lam); glob_i != lam2glob_.end()) {
-        switch (glob_i->second) {
-            case Glob::Eta_Wrap: return Lattice::Eta_Wrap;
-            case Glob::Callee_N: return Lattice::Callee_N;
-        }
-    }
-
-    switch (auto [loc, _, __] = insert<LamMap<Loc>>(lam); loc) {
-        case Loc::Non_Callee_1: return Lattice::Non_Callee_1;
-        case Loc::Beta_Reduced: return Lattice::Beta_Reduced;
-        case Loc::Bot:          return Lattice::Bot;
-    }
-
-    THORIN_UNREACHABLE;
-}
-
 const Def* BetaEtaConv::rewrite(Def*, const Def* def) {
     if (def->isa<Param>() || def->isa<Proxy>()) return def;
 
     for (size_t i = 0, e = def->num_ops(); i != e; ++i) {
         if (auto lam = def->op(i)->isa_nominal<Lam>(); lam != nullptr && !ignore(lam) && !man().is_tainted(lam)) {
-            //  η-reduction: λx.e x -> e, whenever x does not appear free in e
             if (auto app = lam->body()->isa<App>(); app != nullptr && app->arg() == lam->param() && !is_free(lam->param(), app->callee())) {
                 auto new_def = def->refine(i, app->callee());
                 world().DLOG("eta-reduction '{}' -> '{}' by eliminating '{}'", def, new_def, lam);
                 return new_def;
             }
 
-            if (auto new_def = def2eta_.lookup(def)) return *new_def;
-
-            auto app = def->isa<App>();
-            bool callee = app != nullptr && i == 0;
-
-            switch (lattice(lam)) {
-                case Lattice::Bot:
-                    if (callee) {
-                        std::get<Loc&>(insert<LamMap<Loc>>(lam)) = Loc::Beta_Reduced;
-                        world().DLOG("beta-reduction '{}'", lam);
+            if (auto app = def->isa<App>(); app != nullptr && i == 0) {
+                if (auto lam = app->callee()->isa_nominal<Lam>(); !ignore(lam) && !man().is_tainted(lam) && !keep_.contains(lam)) {
+                    if (auto [_, ins] = put<LamSet>(lam); ins) {
+                        world().DLOG("beta-redunction '{}'", lam);
                         return lam->apply(app->arg());
-                    }
-                    break;
-                case Lattice::Beta_Reduced:
-                    if (callee) {
-                        world().DLOG("proxy app '{}'", lam);
+                    } else {
                         return proxy(app->type(), {lam, app->arg()});
                     }
-                    break;
-                default: break;
+                }
+            } else {
+                if (wrappers_.contains(def)) return def;
+
+                auto& eta = def2eta_.emplace(def, nullptr).first->second;
+                if (eta == nullptr) {
+                    auto wrap = lam->stub(world(), lam->type(), lam->debug());
+                    wrap->set_name(std::string("eta_wrap_") + lam->name());
+                    wrap->app(lam, wrap->param());
+                    eta = def->refine(i, wrap);
+                    world().DLOG("eta-wrap '{}' -> '{}' using '{}'", def, eta, wrap);
+                    wrappers_.emplace(eta);
+                }
+
+                return eta;
             }
         }
     }
@@ -78,85 +63,36 @@ const Def* BetaEtaConv::rewrite(Def*, const Def* def) {
     return def;
 }
 
+
 undo_t BetaEtaConv::analyze(Def* cur_nom, const Def* def) {
     auto cur_lam = descend(cur_nom, def);
     if (cur_lam == nullptr) return No_Undo;
 
     if (auto proxy = isa_proxy(def)) {
         auto lam = proxy->op(0)->as_nominal<Lam>();
-        world().DLOG("found proxy app of '{}'", lam);
-
-        auto&& [loc, undo, _] = insert<LamMap<Loc>>(lam);
-        switch (lattice(lam)) {
-            case Lattice::Bot:
-                THORIN_UNREACHABLE;
-            case Lattice::Beta_Reduced:
-                lam2glob_[lam] = Glob::Callee_N;
-                return undo;
-            case Lattice::Non_Callee_1:
-                lam2glob_[lam] = Glob::Eta_Wrap;
-                return undo;
-            case Lattice::Callee_N:
-            case Lattice::Eta_Wrap:
-                return undo;
+        if (keep_.emplace(lam).second) {
+            world().DLOG("found proxy app of '{}'", lam);
+            auto [undo, _] = put<LamSet>(lam);
+            return undo;
         }
-    }
+    } else {
+        auto undo = No_Undo;
+        for (auto op : def->ops()) {
+            undo = std::min(undo, analyze(cur_nom, op));
 
-    auto undo = No_Undo;
-    for (size_t i = 0, e = def->num_ops(); i != e; ++i) {
-        undo = std::min(undo, analyze(cur_nom, def->op(i)));
-
-        if (auto lam = def->op(i)->isa_nominal<Lam>(); lam != nullptr && !ignore(lam) && !man().is_tainted(lam)) {
-            auto&& [loc, u, __] = insert<LamMap<Loc>>(lam);
-            auto app = def->isa<App>();
-            bool callee = app != nullptr && i == 0;
-
-            auto eta_wrap = [&]() {
-                if (auto& eta = def2eta_.emplace(def, nullptr).first->second; eta == nullptr) {
-                    auto wrap = lam->stub(world(), lam->type(), lam->debug());
-                    wrap->set_name(std::string("eta_wrap_") + lam->name());
-                    wrap->app(lam, wrap->param());
-                    eta = def->refine(i, wrap);
-                    world().DLOG("eta-wrap '{}' -> '{}' using '{}'", def, eta, wrap);
-                    undo = std::min(undo, cur_undo());
+            if (auto lam = op->isa_nominal<Lam>(); !ignore(lam) && !man().is_tainted(lam) && keep_.emplace(lam).second) {
+                auto [lam_undo, ins] = put<LamSet>(lam);
+                if (!ins) {
+                    world().DLOG("non-callee-position of '{}'; undo to {} beta-reduction of {} within {}", lam, lam_undo, lam, cur_nom);
+                    undo = std::min(undo, lam_undo);
                 }
-            };
-
-            auto set_eta_wrap = [&]() {
-                lam2glob_[lam] = Glob::Eta_Wrap;
-                world().DLOG("Eta_Wrap: '{}'", lam);
-                undo = std::min(undo, u);
-            };
-
-            switch (lattice(lam)) {
-                case Lattice::Bot:
-                    assert(!callee);
-                    loc = Loc::Non_Callee_1;
-                    world().DLOG("Bot -> Non_Callee_1: '{}'", lam);
-                    break;
-                case Lattice::Beta_Reduced:
-                    assert(!callee);
-                    set_eta_wrap();
-                    eta_wrap();
-                    break;
-                case Lattice::Non_Callee_1:
-                    set_eta_wrap();
-                    if (!callee) eta_wrap();
-                    break;
-                case Lattice::Callee_N:
-                    if (!callee) {
-                        set_eta_wrap();
-                        eta_wrap();
-                    }
-                    break;
-                case Lattice::Eta_Wrap:
-                    if (!callee) eta_wrap();
-                    break;
             }
         }
+
+        return undo;
     }
 
-    return undo;
+    return No_Undo;
 }
 
 }
--- src/thorin/pass/beta_eta_conv.h
+++ src/thorin/pass/beta_eta_conv.h
@@ -11,43 +11,26 @@ namespace thorin {
  * * β-reduction of <code>f e</code>happens if <code>f</code> only occurs exactly once in the program in callee position.
  * * η-reduction:
  *      * <code>λx.e x -> e</code>, whenever <code>x</code> does not appear free in <code>e</code> (always).
- *      * <code>f -> λx.f x</code>, if <code>f</code> is a @p Lam that does not appear in callee position and
- *          * occurs either more than once in non-callee position, or
- *          * appears at least once in calle position as well.
+ *      * <code>f -> λx.f x</code>, if <code>f</code> is a @p Lam that does not appear in callee position.
  *       This rule is a generalization of critical edge elimination.
  *       It gives other @p Pass%es such as @p SSAConstr the opportunity to change <code>f</code>'s signature (e.g. adding or removing params).
- *       The exact rules when which rule is triggered is best understood by the underlying lattice:
- * @code
- *          Eta_Wrap                  <-+
- *          /    \                      |- lam2glob_    - glob(al) lattice
- * Callee_N       \                   <-+
- *         |       \
- *         |      Non_Callee_1        <-+
- * Beta_Reduced   /                   <-+- LambMap<Loc> -  loc(al) lattice
- *          \    /                      |
- *            Bot                     <-+
- * @endcode
  */
+
 class BetaEtaConv : public Pass<BetaEtaConv> {
 public:
     BetaEtaConv(PassMan& man, size_t index)
         : Pass(man, index, "reduction")
     {}
 
-    enum class Lattice { Bot, Beta_Reduced, Non_Callee_1, Callee_N, Eta_Wrap };
-    enum class Loc { Bot, Beta_Reduced, Non_Callee_1 };
-    enum class Glob : uint16_t { Callee_N, Eta_Wrap };
-
     const Def* rewrite(Def*, const Def*) override;
     undo_t analyze(Def*, const Def*) override;
-    Lattice lattice(Lam*);
 
-    using Data = std::tuple<LamMap<Loc>>;
+    using Data = std::tuple<LamSet>;
 
 private:
     LamSet keep_;
-    LamMap<Glob> lam2glob_;
     Def2Def def2eta_;
+    DefSet wrappers_;
 };
 
 }

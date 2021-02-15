e3d50496a0e0c03ca712441cbf50e3ded22e46cf
Hugo Devillers <devillers@cg.uni-saarland.de>
2021-02-15 16:26:32 +0100

    nuke redundant wrapper classes

--- src/thorin/be/c/cuda.cpp
+++
@@ -1,9 +0,0 @@
-#include "cuda.h"
-
-namespace thorin::c {
-
-CUDACodeGen::CUDACodeGen(World& world, const Cont2Config& kernel_config, int opt, bool debug)
-    : CodeGen(world, kernel_config, Lang::CUDA, debug)
-{}
-
-}
--- src/thorin/be/c/hls.cpp
+++
@@ -1,9 +0,0 @@
-#include "hls.h"
-
-namespace thorin::c {
-
-HLSCodeGen::HLSCodeGen(World& world, const Cont2Config& kernel_config, int opt, bool debug)
-    : CodeGen(world, kernel_config, Lang::HLS, debug)
-{}
-
-}

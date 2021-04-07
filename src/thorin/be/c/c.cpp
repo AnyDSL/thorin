2a7c6120b18a01718714496569a417314d6c9286
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2021-04-07 16:21:21 +0200

    Fix spacing

--- src/thorin/be/c/c.cpp
+++ src/thorin/be/c/c.cpp
@@ -877,7 +877,7 @@ std::string CCodeGen::emit_fun_head(Continuation* cont, bool is_proto) {
                 if (!is_proto && config != kernel_config_.end()) {
                     auto block = config->second->as<GPUKernelConfig>()->block_size();
                     if (std::get<0>(block) > 0 && std::get<1>(block) > 0 && std::get<2>(block) > 0)
-                        s.fmt("__launch_bounds__ ({} * {} * {})", std::get<0>(block), std::get<1>(block), std::get<2>(block));
+                        s.fmt("__launch_bounds__({} * {} * {}) ", std::get<0>(block), std::get<1>(block), std::get<2>(block));
                 }
                 break;
             case Lang::OpenCL:
@@ -885,7 +885,7 @@ std::string CCodeGen::emit_fun_head(Continuation* cont, bool is_proto) {
                 if (!is_proto && config != kernel_config_.end()) {
                     auto block = config->second->as<GPUKernelConfig>()->block_size();
                     if (std::get<0>(block) > 0 && std::get<1>(block) > 0 && std::get<2>(block) > 0)
-                        s.fmt("__attribute__((reqd_work_group_size({} * {} * {})))", std::get<0>(block), std::get<1>(block), std::get<2>(block));
+                        s.fmt("__attribute__((reqd_work_group_size({} * {} * {}))) ", std::get<0>(block), std::get<1>(block), std::get<2>(block));
                 }
                 break;
         }

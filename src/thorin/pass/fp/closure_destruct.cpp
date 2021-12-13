d7f522e2ee2a1f554b9cb6232b3cd38e1d2960d2
Fabian Fries <s8fafrie@stud.uni-saarland.de>
2021-12-14 00:18:06 +0100

    ClosureDestruct Wip: Get rid of points-to-graph

--- src/thorin/pass/fp/closure_destruct.cpp
+++ src/thorin/pass/fp/closure_destruct.cpp
@@ -1,130 +1,34 @@
 
-#include <functional>
-
+#include "thorin/transform/closure_conv.h"
 #include "thorin/pass/fp/closure_destruct.h"
-#include "thorin/transform/untype_closures.h"
 
 namespace thorin {
 
-template<class I>
-static inline undo_t min_undo(I& cont, std::function<undo_t (typename I::value_type)> f,
-    undo_t undo = No_Undo) {
-    for (auto elem: cont)
-        undo = std::min(undo, f(elem));
-    return undo;
-}
-
-static inline undo_t min_undo(size_t n, std::function<undo_t (size_t)> f, undo_t undo = No_Undo) {
-    for (size_t i = 0; i < n; i++)
-        undo = std::min(undo, f(i));
-    return undo;
-}
-
-static const Var* isa_var(const Def* def) {
-    if (auto proj = def->isa<Extract>())
-        def = proj->tuple();
-    return def->isa<Var>();
-}
-
-static bool is_exp_imp(Lam *lam) {
-    return lam->is_external() || !lam->is_set();
-}
-
-ClosureDestruct::Node::Node(const Def* def, bool esc, undo_t undo)
-    : repr_(this)
-    , def_(def)
-    , esc_(esc)
-    , undo_(undo)
-    , points_to_() {
-    if (def && def->isa<Var>() && def->type()->isa<Sigma>())
-        assert(false && "created node for hole argument tuple");
-    if (!def || def->is_external()) { // ⊤, external lam or Global
-        esc_ = true;
-    } else if (def->isa<Global>()) {
-        esc_ = true;
-        add_pointee(top(), 0);
-    } else if (auto var = isa_var(def)) {
-        if (auto lam = var->nom()->isa_nom<Lam>(); lam && is_exp_imp(lam)) {
-            esc_ |= !lam->is_set(); // imported lams always escape their args
-            add_pointee(top(), 0);
+const Def* ClosureDestruct::rewrite(const Def* def) {
+    if (auto c = isa_closure(def)) {
+        if (!escape_.contains(c.lam()) && c.env()->type() != world().sigma()) {
+            auto [old_env, new_lam] = clos2dropped_[c.lam()];
+            if (new_lam && c.env() == old_env)
+                return new_lam;
+            // TODO: Mark non-escaping, only lambda-drop bb's
+            auto doms = world().sigma(Array<const Def*>(c.lam()->num_doms(), [&](auto i) {
+                return (i == 0) ? world().sigma() : c.lam()->dom(i);
+            }));
+            new_lam = c.lam()->stub(world(), world().cn(doms), c.lam()->dbg());
+            world().DLOG("drop ({}, {}) => {}", c.env(), c.lam(), new_lam);
+            auto new_vars = Array<const Def*>(new_lam->num_doms(), [&](auto i) {
+                return (i == 0) ? c.env() : new_lam->var(i); 
+            });
+            new_lam->set(c.lam()->apply(world().tuple(new_vars)));
+            return world().tuple(c.type(), {world().tuple(), new_lam}, def->dbg());
+            // }
         }
     }
-}
-
-undo_t ClosureDestruct::Node::mark_esc() {
-    if (!is_repr())
-        return repr()->mark_esc();
-    if (is_esc())
-        return No_Undo;
-    esc_ = true;
-    auto undo = min_undo(points_to_, [&] (auto edge) {
-        return edge->mark_esc();
-    }, undo_);
-    return undo;
-}
-
-undo_t ClosureDestruct::Node::add_pointee(Node* pointee, size_t iter) {
-    if (!is_repr())
-        return repr()->add_pointee(pointee, iter);
-    if (def_) {
-        auto [p, inserted] = points_to_.emplace(pointee->repr(), iter);
-        if (!inserted) {
-            p->set_iter(iter);
-            return No_Undo;
-        }
-    }
-    return (is_esc()) ? pointee->mark_esc() : No_Undo;
-}
-
-undo_t ClosureDestruct::Node::unify(Node* other) {
-    auto a = repr();
-    auto b = other->repr();
-    if (a == b)
-        return No_Undo;
-    auto res = No_Undo;
-    if (!a->is_esc())
-        std::swap(a, b);
-    if (a->esc_ != b->esc_)
-        res = b->mark_esc();
-    a->esc_ &= b->is_esc();
-    a->undo_ = std::min(a->undo_, b->undo_);
-    a->points_to_.insert(b->points_to_.begin(), b->points_to_.end());
-    a->points_to_.insert({b, 0});
-    b->repr_ = a;
-    b->points_to_.clear();
-    return res;
-}
-
-ClosureDestruct::Node ClosureDestruct::Node::top_(nullptr);
-
-void ClosureDestruct::Node::dump(Stream& s, std::set<Node*>& visited) {
-    if (!def_) {
-        s.fmt("<top>");
-    } else {
-        s.fmt("[{}, {}, {}] \n", repr(), def_, (is_esc()) ? "⊤" : "⊥");
-        if (visited.count(this) == 0) {
-            visited.insert(this);
-            s.indent(1);
-            s.fmt("\n");
-            for (auto& edge: points_to_) {
-                s.fmt("[{}] {} ", edge.iter(), (edge->repr() == this) ? "=" : "->");
-                edge->dump(s, visited);
-            }
-            s.fmt("\n");
-            s.dedent(1);
-        }
-    }
-}
-
-ClosureDestruct::Node* ClosureDestruct::get_node(const Def* def, undo_t undo) { 
-    auto [p, inserted] = def2node_.emplace(def, nullptr);
-    if (inserted)
-        p->second = std::make_unique<Node>(def, false, undo);
-    return p->second.get();
+    return def;
 }
 
 static bool interesting_type_b(const Def* type) {
-    return UntypeClosures::isa_pct(type) != nullptr;
+    return isa_pct(type) != nullptr;
 }
 
 static bool interesting_type(const Def* type) {
@@ -137,120 +41,93 @@ static bool interesting_type(const Def* type) {
     return false;
 }
 
-undo_t ClosureDestruct::add_pointee(ClosureDestruct::Node* node, const Def* def) {
-    if (def->isa_nom<Lam>()) {
-        return node->add_pointee(get_node(def), iter_);
-    } else if (auto var = def->isa<Var>()) {
-        return node->add_pointee(get_node(var), iter_);
-    } else if (auto proj = def->isa<Extract>()) {
+static std::pair<const Def*, Def*> isa_var(const Def* a) {
+    if (auto proj = a->isa<Extract>()) {
         if (auto var = proj->tuple()->isa<Var>(); var && var->nom()->isa<Lam>())
-            return interesting_type(def->type())
-                ? node->add_pointee(get_node(def), iter_) : No_Undo;
-        return add_pointee(node, proj->tuple());
-    } else if (auto closure = UntypeClosures::isa_closure(def)) {
-        return add_pointee(node, closure->op(1_u64));
-    } else if (auto pack = def->isa<Pack>()) {
-        return add_pointee(node, pack->body());
-    } else if (auto tuple = def->isa<Tuple>()) {
-        auto undo = No_Undo;
-        for (auto op: tuple->ops())
-            undo = std::min(undo, add_pointee(node, op));
-        return undo;
-    } else {
-        return No_Undo;
+            return {a, var->nom()};
     }
-}
-
-const Def* ClosureDestruct::rewrite(const Def* def) {
-    if (auto closure = UntypeClosures::isa_closure(def)) {
-        auto env = closure->op(0);
-        auto lam = closure->op(1)->isa_nom<Lam>();
-        auto lam_node = get_node(lam);
-        if (!lam_node->is_esc() && lam->dom(0) != world().sigma()) { // TODO: Check Basic-Block-Like
-            auto& [old_env, dropped] = clos2dropped_[lam];
-            if (!dropped || old_env != env) {
-                auto doms = world().sigma(Array<const Def*>(lam->num_doms(), [&](auto i) {
-                    return (i == 0) ? world().sigma() : lam->dom(i);
-                }));
-                dropped = lam->stub(world(), world().cn(doms), lam->dbg());
-                world().DLOG("drop ({}, {}) => {}", env, lam, dropped);
-                auto new_vars = Array<const Def*>(dropped->num_doms(), [&](auto i) {
-                    return (i == 0) ? env : dropped->var(i); 
-                });
-                dropped->set(lam->apply(world().tuple(new_vars)));
-                auto dropped_node = get_node(dropped, curr_undo());
-                lam_node->unify(dropped_node);
-            }
-            return world().tuple(closure->type(), {world().tuple(), dropped}, closure->dbg());
+    if (auto var = a->isa<Var>()) {
+        if (auto lam = var->nom()->isa<Lam>()) {
+            assert(lam->num_doms() == 1 && "Analyzed whole arg tuple");
+            return {a, var->nom()};
         }
     }
-    return def;
+    return {nullptr, nullptr};
 }
 
-
-// TODO: Interprocedural part?
-undo_t ClosureDestruct::analyze_call(const Def* callee, ArgVec& args) {
-    if (auto lam = callee->isa_nom<Lam>()) {
-        world().DLOG("call {}", lam);
-        return min_undo(args, [&] (auto arg) { 
-            world().DLOG("call: {} -> {}", lam, arg.second);
-            return add_pointee(lam->var(arg.first), arg.second);
-        });
-    } else if (auto closure = UntypeClosures::isa_closure(callee)) {
-        return analyze_call(closure->op(1_u64), args);
-    } else if (auto proj = callee->isa<Extract>()) {
-        return analyze_call(proj->tuple(), args);
-    } else if (auto pack = callee->isa<Pack>()) {
-        return analyze_call(pack->body(), args);
-    } else if (auto tuple = callee->isa<Tuple>()) {
-        auto undo = No_Undo;
+static void split(DefSet& out, const Def* def) {
+    if (auto lam = def->isa<Lam>())
+        out.insert(def);
+    else if (auto c = isa_closure(def))
+        out.insert(c.lam());
+    else if (auto [var, lam] = isa_var(def); var && lam)
+        out.insert(var);
+    else if (auto proj = def->isa<Extract>())
+        split(out, proj->tuple());
+    else if (auto pack = def->isa<Pack>())
+        split(out, pack->body());
+    else if (auto tuple = def->isa<Tuple>())
         for (auto op: tuple->ops())
-            undo = std::min(undo, analyze_call(op, args));
-        return undo;
-    } else {
-        return min_undo(args, [&] (auto arg) {
-            world().DLOG("call {}: escape {}", callee, arg.second);
-            return add_pointee(Node::top(), arg.second);
-        });
-    }
+            split(out, op);
 }
 
-// TODO: Analyze store
-undo_t ClosureDestruct::analyze(const Def* def) {
+static DefSet&& split(DefSet&& out, const Def* def) {
+    split(out, def);
+    return std::move(out);
+}
+
+undo_t ClosureDestruct::join(DefSet& out, DefSet& defs, bool cond) {
+    if (!cond)
+        return No_Undo;
     auto undo = No_Undo;
-    if (auto closure = UntypeClosures::isa_closure(def)) {
-        undo = add_pointee(closure->op(1), closure->op(0));
-        // FIXME: env also flows into the env-argument: #x @{f, 0} for 0 <= x <= size env
-    } else if (auto app = def->isa<App>(); app && app->callee_type()->is_cn()) {
-        auto args = ArgVec();
-        for (size_t i = 0; i < app->num_args(); i++) 
-            if (interesting_type(app->callee_type()->dom(i)))
-                args.emplace_back(i, app->arg(i));
-        undo = analyze_call(app->callee(), args);
+    for (auto def: defs) {
+        if (out.contains(def))
+            continue;
+        out.insert(def);
+        if (auto [_, lam] = isa_var(def); lam) {
+            undo = std::min(undo, undo_visit(lam));
+        } else {
+            lam = def->isa_nom<Lam>();
+            assert(lam);
+            undo = std::min(undo, undo_visit(lam));
+        }
     }
     return undo;
 }
 
-void ClosureDestruct::unify(const Def* a, const Def* b) {
-    get_node(a)->unify(get_node(b));
+undo_t ClosureDestruct::join(DefSet& out, const Def* def, bool cond) {
+    if (!cond)
+        return No_Undo;
+    auto defs = DefSet();
+    split(defs, def);
+    return join(out, defs, cond);
 }
 
-void ClosureDestruct::dump_node(Node* node) {
-    world().stream().fmt("{}\n", *node);
-}
-
-void ClosureDestruct::dump_graph() {
-    auto s = world().stream();
-    auto v = std::set<Node*>();
-    s.fmt("-----------------\n");
-    for (auto& [def, node]: def2node_) {
-        if (v.count(node.get()) == 0) {
-            s.fmt("{} =>\n", def);
-            node->dump(s, v);
-            s.fmt("\n");
+undo_t ClosureDestruct::analyze(const Def* def) {
+    if (auto c = isa_closure(def)) {
+        return join(escape_, c.env(), is_esc(c.lam()) && is_esc(c.env_var()));
+    } else if (auto app = def->isa<App>()) {
+        auto callees = split(DefSet(), def);
+        auto undo = No_Undo;
+        for (size_t i = 0; i < app->num_args(); i++) {
+            auto a = app->arg(i);
+            if (!interesting_type(a->type()))
+                continue;
+            auto cond = std::any_of(callees.begin(), callees.end(), [&](const Def* c) {
+                if (auto [var, lam] = isa_var(c); var && lam)
+                    return true;
+                auto lam = c->isa_nom<Lam>();
+                assert(lam && "callee should be lam or var");
+                return is_esc(lam->var(i));
+            });
+            if (!cond)
+                continue;
+            auto args = split(DefSet(), app->arg(i));
+            undo = std::min(undo, join(escape_, args, true));
         }
+        return undo;
     }
-    s.fmt("\n");
+    return No_Undo;
 }
 
 } // namespace thorin
--- src/thorin/pass/fp/closure_destruct.h
+++ src/thorin/pass/fp/closure_destruct.h
@@ -2,7 +2,8 @@
 #define THORIN_CLOSURE_DESTRUCT_H
 
 #include <set>
-#include <map>
+#include <functional>
+
 #include "thorin/pass/pass.h"
 
 namespace thorin {
@@ -13,15 +14,8 @@ class ClosureDestruct : public FPPass<ClosureDestruct, Lam> {
 public:
     ClosureDestruct(PassMan& man) 
         : FPPass<ClosureDestruct, Lam>(man, "closure_destruct")
-        , iter_(1), clos2dropped_() 
+        , escape_(), clos2dropped_()
     {}
-
-    void unify(const Def* a, const Def* b);
-
-    void enter() override { 
-        iter_++; 
-        dump_graph();
-    }
 
     const Def* rewrite(const Def*) override;
     undo_t analyze(const Def*) override;
@@ -29,113 +23,16 @@ public:
     using Data = int;
 
 private:
-    class Edge;
-    class Node;
-
-    Node* get_node(const Def* def, undo_t undo = No_Undo);
-
-    undo_t add_pointee(Node* node, const Def* def);
-    undo_t add_pointee(const Def* a, const Def* b, undo_t undo = No_Undo) {
-        return add_pointee(get_node(a, undo), b);
-    }
-
-    using ArgVec = std::vector<std::pair<size_t, const Def*>>;
-    undo_t analyze_call(const Def* callee, ArgVec& vec);
-
-    void dump_node(Node* node);
-    void dump_graph();
-
-    size_t iter_;
-    DefMap<std::unique_ptr<Node>> def2node_;
+    DefSet escape_;
     LamMap<std::pair<const Def*, Lam*>> clos2dropped_;
+
+    bool is_esc(const Def* def) {
+        return escape_.contains(def);
+    }
+
+    undo_t join(DefSet& out, DefSet& defs, bool cond);
+    undo_t join(DefSet& out, const Def* def, bool cond);
 };
-
-struct ClosureDestruct::Edge {
-public:
-    Edge(Node* node, size_t iter)
-        : node_(node), iter_(iter) {};
-
-    Node* operator*() const {
-        return node_;
-    }
-
-    Node* operator->() const {
-        return node_;
-    }
-
-    friend bool operator<(Edge a, Edge b) {
-        return a.node_ < b.node_;
-    }
-
-    friend bool operator==(Edge a, Edge b) {
-        return a.node_ == b.node_;
-    }
-
-    size_t iter() const { return iter_; }
-    void set_iter(size_t new_iter) const {
-        iter_ = std::min(iter_, new_iter);
-    }
-
-    void dump(Stream& s, std::set<Node*>& visited) const;
-    
-private:
-    Node* const node_;
-    mutable size_t iter_; 
-};
-
-
-class ClosureDestruct::Node {
-public:
-    Node(const Def* def, bool esc = false, undo_t undo = No_Undo);
-
-    template<class T = const Def>
-    T* def() { assert(def_); return def_->isa<T>(); };
-
-    inline bool is_repr() {
-        return this == repr_;
-    }
-
-    Node* repr() {
-        if (!is_repr())
-            repr_ = repr_->repr();
-        return repr_;
-    }
-
-    bool is_esc() { 
-        if (!is_repr())
-            return repr()->is_esc();
-        return esc_; 
-    }
-
-    undo_t mark_esc();
-
-    undo_t add_pointee(Node* pointee, size_t iter);
-
-    undo_t unify(Node* other);
-
-    void dump(Stream& s, std::set<Node*>& visited);
-
-    friend Stream& operator<<(Stream& s, Node& node) {
-        auto visited = std::set<Node*>();
-        node.dump(s, visited); 
-        return s;
-    }
-
-    static Node* top() { return &top_; }
-
-private:
-    Node* repr_;
-    const Def* def_;
-    bool esc_;
-    undo_t undo_;
-    std::set<Edge> points_to_;
-
-    static Node top_; 
-};
-
 
 }
-
-
-
 #endif

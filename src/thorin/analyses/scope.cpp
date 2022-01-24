510d3584adbcb176b756d954f7e38627d45d4eea
Hugo Devillers <devillers@cg.uni-saarland.de>
2022-01-24 12:45:47 +0100

    suppress errors caused by malformed (yet dead) IR

--- src/thorin/analyses/scope.cpp
+++ src/thorin/analyses/scope.cpp
@@ -84,7 +84,7 @@ const ParamSet& Scope::free_params() const {
         unique_queue<DefSet> queue;
 
         auto enqueue = [&](const Def* def) {
-            if (auto param = def->isa<Param>())
+            if (auto param = def->isa<Param>(); param && !param->continuation()->dead_)
                 free_params_->emplace(param);
             else if (def->isa<Continuation>())
                 return;

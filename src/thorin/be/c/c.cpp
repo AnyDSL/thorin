2d8d0b8fc31a2c7a1626f87d76c1940cffaa3cec
Richard Membarth <richard.membarth@uni-saarland.de>
2022-02-18 18:10:49 +0100

    Add typedefs for C interface generation.

--- src/thorin/be/c/c.cpp
+++ src/thorin/be/c/c.cpp
@@ -1497,6 +1497,17 @@ void CCodeGen::emit_c_int() {
     stream_.fmt("extern \"C\" {{\n");
     stream_.fmt("#endif\n\n");
 
+    stream_.fmt("typedef   int8_t  i8;\n"
+                "typedef  uint8_t  u8;\n"
+                "typedef  int16_t i16;\n"
+                "typedef uint16_t u16;\n"
+                "typedef  int32_t i32;\n"
+                "typedef uint32_t u32;\n"
+                "typedef  int64_t i64;\n"
+                "typedef uint64_t u64;\n"
+                "typedef    float f32;\n"
+                "typedef   double f64;\n\n");
+
     if (!type_decls_.str().empty())
         stream_.fmt("{}\n", type_decls_.str());
     if (!func_decls_.str().empty())

bdc6df4da9128a2c4a1b16036e5a60499ac2d378
Puya Amiri <pooya.w@gmail.com>
2022-09-12 15:55:34 +0200

    preparing structures for rewriting CGRA kernels

--- src/thorin/transform/cgra_graphs.cpp
+++ src/thorin/transform/cgra_graphs.cpp
@@ -13,13 +13,44 @@ namespace thorin {
 void cgra_graphs(Importer& importer) {
 
     auto& world = importer.world();
+    Def2Def kernel_new2old;
+    std::vector<Continuation*> new_kernels;
     //world.dump();
-
     Scope::for_each(world, [&] (Scope& scope) {
-        Def2Mode def2mode;
+        Def2Mode def2mode; // channels and their R/W modes
         extract_kernel_channels(schedule(scope), def2mode);
-        for (auto elem : def2mode)
-                elem.first->dump();
+
+        auto old_kernel = scope.entry();
+        Array<const Type*> new_param_types(def2mode.size() + old_kernel->num_params());
+            std::copy(old_kernel->type()->ops().begin(),
+                    old_kernel->type()->ops().end(),
+                    new_param_types.begin());
+
+            size_t channel_index = old_kernel->num_params();
+            // The position of the channel parameter for the new kernel and channel defintion
+            std::vector<std::pair<size_t, const Def*>> channel_param_index2def;
+            for (auto [channel, _ ]: def2mode) {
+                channel_param_index2def.emplace_back(channel_index, channel);
+                new_param_types[channel_index++] = channel->type();
+            }
+
+            // new kernels signature
+            // fn(mem, ret_cnt, ... , /channels/ )
+            auto new_kernel = world.continuation(world.fn_type(new_param_types), old_kernel->debug());
+            world.make_external(new_kernel);
+
+            kernel_new2old.emplace(new_kernel, old_kernel);
+
+            // Kernels without any channels are scheduled in the begening
+            if (is_single_kernel(new_kernel))
+                new_kernels.emplace(new_kernels.begin(),new_kernel);
+            else
+                new_kernels.emplace_back(new_kernel);
+
+            world.make_internal(old_kernel);
+
+            Rewriter rewriter;
+
     });
 
     world.cleanup();

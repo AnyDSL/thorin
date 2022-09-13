1d3874442df48a913e47fc1b6d0032de3e394557
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2022-09-13 16:26:27 +0200

    Use RTLD_GLOBAL to enable loading dependent plugins.

--- src/thorin/world.cpp
+++ src/thorin/world.cpp
@@ -1308,7 +1308,7 @@ void World::opt() {
 }
 
 bool World::register_plugin(std::string plugin_name) {
-    void *handle = dlopen(plugin_name.c_str(), RTLD_LAZY);
+    void *handle = dlopen(plugin_name.c_str(), RTLD_LAZY | RTLD_GLOBAL);
     if (!handle) {
         ELOG("Error loading plugin {}: {}", plugin_name, dlerror());
         ELOG("Is plugin contained in LD_LIBRARY_PATH?");

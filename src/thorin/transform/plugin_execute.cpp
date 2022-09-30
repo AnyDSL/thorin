baafbe98602983b96490b8a8cb7bab9bb4a4b754
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2022-09-30 16:01:47 +0200

    Add plugin dependencies + option to return values from plugin intrinsics.

--- src/thorin/transform/plugin_execute.cpp
+++ src/thorin/transform/plugin_execute.cpp
@@ -2,43 +2,57 @@
 #include "thorin/transform/plugin_execute.h"
 #include "thorin/analyses/scope.h"
 
+#include <vector>
+
 namespace thorin {
 
 void plugin_execute(World& world) {
     world.VLOG("start plugin_execute");
 
+    std::vector<const Continuation*> plugin_intrinsics;
+
     for (auto cont : world.copy_continuations()) {
         if (cont->is_intrinsic() && cont->intrinsic() == Intrinsic::Plugin) {
-            void * function_handle = world.search_plugin_function(cont->name());
-            if (!function_handle) {
-                world.ELOG("Plugin function not found for: {}", cont->name());
+            plugin_intrinsics.push_back(cont);
+            if (cont->attributes().depends) {
+                cont->dump();
+                cont->attributes().depends->dump();
+            }
+        }
+    }
+
+    sort(plugin_intrinsics.begin(), plugin_intrinsics.end(), [&](const Continuation* a, const Continuation* b) {
+            const Continuation* depends = a;
+            while (depends) {
+              if (depends == b) return false;
+              depends = depends->attributes().depends;
+            }
+            return true;
+    });
+
+    for (auto cont : plugin_intrinsics) {
+        void * function_handle = world.search_plugin_function(cont->name());
+        if (!function_handle) {
+            world.ELOG("Plugin function not found for: {}", cont->name());
+            continue;
+        }
+        auto plugin_function = (void*(*)(size_t, void**)) function_handle;
+
+        for (auto use : cont->copy_uses()) {
+            if (!use.def()->isa<App>()) {
                 continue;
             }
-            auto plugin_function = (void*(*)(size_t, void**)) function_handle;
 
-            for (auto use : cont->copy_uses()) {
-                if (!use.def()->isa<App>()) {
-                    continue;
-                }
+            auto app = const_cast<App*>(use.def()->as<App>());
 
-                auto app = const_cast<App*>(use.def()->as<App>());
-
-                Def* input_array[app->num_args() - 2];
-                for (size_t i = 1, e = app->num_args() - 1; i < e; i++) {
-                    Def * input = const_cast<Def*>(app->arg(i));
-                    input_array[i - 1] = input;
-                }
-
-                void * output = plugin_function(app->num_args() - 2, (void **)input_array);
-
-                Continuation* y = world.continuation(world.fn_type({world.mem_type(), world.fn_type({world.mem_type()})}));
-                y->jump(y->param(1), {y->param(0)});
-
-                Continuation* x = world.continuation(world.fn_type({world.mem_type()}));
-                x->jump(app->arg(app->num_args() - 1), {x->param(0), y});
-
-                app->jump((Def*)output, {app->arg(0), x});
+            Def* input_array[app->num_args() - 2];
+            for (size_t i = 1, e = app->num_args() - 1; i < e; i++) {
+                Def * input = const_cast<Def*>(app->arg(i));
+                input_array[i - 1] = input;
             }
+
+            void * output = plugin_function(app->num_args() - 2, (void **)input_array);
+            app->jump(app->arg(app->num_args() - 1), {app->arg(0), (Def*)output});
         }
     }
 

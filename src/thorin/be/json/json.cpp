841c2086d37f627e23a0139f31ef82f6d1d3910b
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2022-11-14 15:18:40 +0100

    [Json] various improvements:
      * Avoid name collisions, generate names after args have been created.
      * Querry known_defs to avoid multiple instances of the same definition.
      * Deal with intrinsics. Limited to branch and named intrinsics for now.
      * Create Cmp defs.

--- src/thorin/be/json/json.cpp
+++ src/thorin/be/json/json.cpp
@@ -68,37 +68,52 @@ public:
     DefMap<std::string> known_defs;
 
     std::string translate_def (const Def * def, std::string expected_name = "") {
+        auto it = known_defs.find(def);
+        if (it != known_defs.end()) {
+            return it->second;
+        }
+
         json result;
         if (auto cont = def->isa<Continuation>()) {
-            auto type = type_table_.translate_type(def->type());
-            auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
-            json arg_names;
-            for (auto arg : cont->params()) {
-                arg_names.push_back(translate_def(arg));
+            if (cont->is_intrinsic()) {
+                assert(cont->intrinsic() == Intrinsic::Branch && "TODO: anything else is unsupported RN");
+
+                result["name"] = "branch";
+                result["type"] = "continuation";
+                result["intrinsic"] = "branch";
+            } else {
+                assert(cont->has_body());
+
+                auto type = type_table_.translate_type(def->type());
+                json arg_names = json::array();
+                for (auto arg : cont->params()) {
+                    arg_names.push_back(translate_def(arg));
+                }
+
+                auto name = expected_name != "" ? expected_name : "_cont_" + std::to_string(decl_table.size());
+
+                json forward_decl;
+                forward_decl["name"] = name;
+                forward_decl["type"] = "continuation";
+                forward_decl["fn_type"] = type;
+                forward_decl["arg_names"] = arg_names;
+                forward_decl["external"] = cont->is_external();
+                decl_table.push_back(forward_decl);
+
+                auto app = cont->body();
+                auto target = translate_def(app->callee());
+                json args = json::array();
+                for (auto arg : app->args()) {
+                    args.push_back(translate_def(arg));
+                }
+
+                result["name"] = name;
+                result["type"] = "continuation";
+                result["app"] = {
+                    {"target", target},
+                    {"args", args}
+                };
             }
-
-            json forward_decl;
-            forward_decl["name"] = name;
-            forward_decl["type"] = "continuation";
-            forward_decl["fn_type"] = type;
-            forward_decl["arg_names"] = arg_names;
-            forward_decl["external"] = cont->is_external();
-            decl_table.push_back(forward_decl); //TODO: should be pushed to the front instead.
-
-            assert(cont->has_body());
-            auto app = cont->body();
-            auto target = translate_def(app->callee());
-            json args = json::array();
-            for (auto arg : app->args()) {
-                args.push_back(translate_def(arg));
-            }
-
-            result["name"] = name;
-            result["type"] = "continuation";
-            result["app"] = {
-                {"target", target},
-                {"args", args}
-            };
         } else if (auto lit = def->isa<PrimLit>()) {
             auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
             auto type = type_table_.translate_type(def->type());
@@ -112,50 +127,61 @@ public:
             known_defs[def] = name;
             return name;
         } else if (auto load = def->isa<Load>()) {
-            auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
             json args = json::array();
             args.push_back(translate_def(load->mem()));
             args.push_back(translate_def(load->ptr()));
+            auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
 
             result["name"] = name;
             result["type"] = "load";
             result["args"] = args;
         } else if (auto cast = def->isa<Cast>()) {
-            auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
             auto source = translate_def(cast->from());
             auto target_type = type_table_.translate_type(cast->type());
+            auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
 
             result["name"] = name;
             result["type"] = "cast";
             result["source"] = source;
             result["target_type"] = target_type;
         } else if (auto lea = def->isa<LEA>()) {
-            auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
             json args = json::array();
             args.push_back(translate_def(lea->ptr()));
             args.push_back(translate_def(lea->index()));
+            auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
 
             result["name"] = name;
             result["type"] = "lea";
             result["args"] = args;
         } else if (auto extract = def->isa<Extract>()) {
-            auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
             json args = json::array();
             args.push_back(translate_def(extract->agg()));
             args.push_back(translate_def(extract->index()));
+            auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
 
             result["name"] = name;
             result["type"] = "extract";
             result["args"] = args;
         } else if (auto arithop = def->isa<ArithOp>()) {
-            auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
             auto op = arithop->op_name();
             json args = json::array();
             args.push_back(translate_def(arithop->lhs()));
             args.push_back(translate_def(arithop->rhs()));
+            auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
 
             result["name"] = name;
             result["type"] = "arithop";
+            result["op"] = op;
+            result["args"] = args;
+        } else if (auto cmp = def->isa<Cmp>()) {
+            auto op = cmp->op_name();
+            json args = json::array();
+            args.push_back(translate_def(cmp->lhs()));
+            args.push_back(translate_def(cmp->rhs()));
+            auto name = expected_name != "" ? expected_name : "_" + std::to_string(def_table.size());
+
+            result["name"] = name;
+            result["type"] = "cmp";
             result["op"] = op;
             result["args"] = args;
         } else {
@@ -186,8 +212,6 @@ void CodeGen::emit_stream(std::ostream& stream) {
     for (auto it : def_table.def_table)
         j["defs"] += it;
     
-    std::cerr << j.dump(2) << std::endl;
-
     Stream s(stream);
     s << j.dump(2) << "\n";
 }

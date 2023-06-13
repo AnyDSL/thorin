4eefa459a5e90b88ec747419be43c8c96d5e5ad7
Richard Membarth <richard.membarth@uni-saarland.de>
2023-06-13 11:11:48 +0200

    PAL: set kernel calling convention to AMDGPU_CS.

--- src/thorin/be/llvm/amdgpu_pal.cpp
+++ src/thorin/be/llvm/amdgpu_pal.cpp
@@ -8,7 +8,7 @@
 namespace thorin::llvm {
 
 AMDGPUPALCodeGen::AMDGPUPALCodeGen(World& world, const Cont2Config& kernel_config, int opt, bool debug)
-    : CodeGen(world, llvm::CallingConv::C, llvm::CallingConv::C, llvm::CallingConv::AMDGPU_KERNEL, opt, debug)
+    : CodeGen(world, llvm::CallingConv::C, llvm::CallingConv::C, llvm::CallingConv::AMDGPU_CS, opt, debug)
     , kernel_config_(kernel_config)
 {
     module().setDataLayout("e-p:64:64-p1:64:64-p2:32:32-p3:32:32-p4:64:64-p5:32:32-p6:32:32-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024-v2048:2048-n32:64-S32-A5-ni:7");

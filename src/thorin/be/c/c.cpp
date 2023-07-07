c3712bdbe83cbf8b6e41435a1f373ac5d4472e5a
Hugo Devillers <devillers@cg.uni-saarland.de>
2023-07-07 15:56:35 +0200

    fix c backend emitting int literal suffixes as part of member names

--- src/thorin/be/c/c.cpp
+++ src/thorin/be/c/c.cpp
@@ -1033,7 +1033,7 @@ void CCodeGen::emit_access(Stream& s, const Type* agg_type, const Def* index, co
     } else if (agg_type->isa<IndefiniteArrayType>()) {
         s.fmt("[{}]", emit(index));
     } else if (agg_type->isa<TupleType>()) {
-        s.fmt("{}e{}", prefix, emit_constant(index));
+        s.fmt("{}e{}", prefix, primlit_value<size_t>(index));
     } else if (agg_type->isa<StructType>()) {
         s.fmt("{}{}", prefix, agg_type->as<StructType>()->op_name(primlit_value<size_t>(index)));
     } else if (agg_type->isa<VectorType>()) {

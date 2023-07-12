fb284ce0b2339aed9823bc887a1aab2beda7ea85
Hugo Devillers <devillers@cg.uni-saarland.de>
2023-07-12 13:51:07 +0200

    disable codegen_prepare and add a cleanup pass after lower_control

--- src/thorin/world.cpp
+++ src/thorin/world.cpp
@@ -1355,10 +1355,11 @@ void Thorin::opt() {
     //RUN_PASS(hoist_enters(*this))
     //RUN_PASS(lift2cff(*this, false))
     RUN_PASS(lower_control(*this));
+    RUN_PASS(cleanup())
     RUN_PASS(closure_conversion(*this));
     //RUN_PASS(lower_return(*this));
     RUN_PASS(cleanup())
-    RUN_PASS(codegen_prepare(*this))
+    //RUN_PASS(codegen_prepare(*this))
 }
 
 }

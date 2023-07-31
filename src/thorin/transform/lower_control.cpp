b983042d55b92abbc36aaa77263436068df68081
Hugo Devillers <devillers@cg.uni-saarland.de>
2023-07-31 12:50:12 +0200

    lower_control: more general implementation

--- src/thorin/transform/lower_control.cpp
+++ src/thorin/transform/lower_control.cpp
@@ -20,15 +20,21 @@ struct LowerControl : Rewriter {
         if (auto oapp = odef->isa<App>()) {
             auto ocallee = oapp->callee()->isa_nom<Continuation>();
             if (ocallee && (ocallee->intrinsic() == Intrinsic::Control || ocallee->intrinsic() == Intrinsic::ControlStatic)) {
-                auto obody = oapp->arg(1)->as<Continuation>();
-                assert(oapp->arg(1)->uses().size() == 3);
                 auto nmem = instantiate(oapp->arg(0));
+                auto nbody = instantiate(oapp->arg(1)->as<Continuation>());
                 auto npost = instantiate(oapp->arg(2));
-                insert(obody->param(0), nmem);
-                insert(obody->param(1), npost);
-                return instantiate(obody->body());
-                //auto nbody = instantiate(oapp->arg(1));
-                //return dst().run(dst().app(nbody, { nmem, wrapped_post }));
+
+                if (!npost->type()->isa<ClosureType>()) {
+                    auto wrapper = dst().continuation(npost->type()->as<FnType>());
+                    wrapper->jump(npost, wrapper->params_as_defs());
+                    auto closure_t = dst().closure_type(wrapper->type()->types());
+                    wrapper->append_param(closure_t);
+                    auto closure = dst().closure(closure_t);
+                    closure->set_fn(wrapper);
+                    closure->set_env(dst().tuple({}));
+                    npost = closure;
+                }
+                return dst().run(dst().app(nbody, {nmem, npost }));
             }
         }
 

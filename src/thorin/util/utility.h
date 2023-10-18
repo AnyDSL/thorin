1c5b75ce67899c82334619c7b175b204fdff10d6
Puya Amiri <pooya.w@gmail.com>
2023-10-18 15:10:49 +0200

    underlying_type util

--- src/thorin/util/utility.h
+++ src/thorin/util/utility.h
@@ -236,6 +236,12 @@ inline uint64_t pad(uint64_t offset, uint64_t align) {
 
 //@}
 
+/// Returns the underlying value of an enum.
+template <typename E>
+constexpr typename std::underlying_type<E>::type to_underlying(E e) {
+    return static_cast<typename std::underlying_type<E>::type>(e);
+}
+
 }
 
 #endif

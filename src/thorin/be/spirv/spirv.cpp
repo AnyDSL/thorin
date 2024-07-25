cfa14c5eaa53d3fcef76c2037d20c6c1b3f70a79
Hugo Devillers <devillers@cg.uni-saarland.de>
2024-07-25 15:00:13 +0200

    commented out weird LEA address space stuff

--- src/thorin/be/spirv/spirv.cpp
+++ src/thorin/be/spirv/spirv.cpp
@@ -631,14 +631,14 @@ SpvId CodeGen::emit_bb(BasicBlockBuilder* bb, const Def* def) {
     } else if (auto enter = def->isa<Enter>()) {
         return emit_unsafe(enter->mem());
     } else if (auto lea = def->isa<LEA>()) {
-        switch (lea->type()->addr_space()) {
-            case AddrSpace::Global:
-            case AddrSpace::Shared:
-                break;
-            default:
-                world().ELOG("LEA is only allowed in global & shared address spaces");
-                break;
-        }
+        //switch (lea->type()->addr_space()) {
+        //    case AddrSpace::Global:
+        //    case AddrSpace::Shared:
+        //        break;
+        //    default:
+        //        world().ELOG("LEA is only allowed in global & shared address spaces");
+        //        break;
+        //}
         auto type = convert(lea->type()).id;
         auto offset = emit(lea->index());
         return bb->ptr_access_chain(type, emit(lea->ptr()), offset, {});

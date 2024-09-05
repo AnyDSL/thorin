b438068f8ce109f81d030c488f33a9a8370393a3
Puya Amiri <pooya.w@gmail.com>
2024-09-05 19:51:46 +0200

    use kernel file name instead of address

--- src/thorin/be/c/c.cpp
+++ src/thorin/be/c/c.cpp
@@ -18,6 +18,7 @@
 #include <unordered_map> // TODO don't use std::unordered_*
 #include <unordered_set>
 #include <variant>
+#include <filesystem>
 
 namespace thorin::c {
 
@@ -973,7 +974,8 @@ void CCodeGen::graph_ctor_gen (const Continuations& graph_conts) {
                 if (loc_x >= 0 && loc_y >= 0)
                     configs_.fmt( "adf::location<adf::kernel>({}) = adf::tile({}, {});\n", krl_node_name(callee), loc_x, loc_y);
 
-                configs_.fmt( "adf::source({}) = \"{}_kernel{}\";\n", krl_node_name(callee), world().name(), source_ext);
+                auto source_name = std::filesystem::path(world().name()).filename().string();
+                configs_.fmt( "adf::source({}) = \"{}_kernel{}\";\n", krl_node_name(callee), source_name, source_ext);
             }
         }
     }

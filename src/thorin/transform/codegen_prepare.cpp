edfd566619c996a908d8e7ef4b96632c4c83d297
Hugo Devillers <devillers@cg.uni-saarland.de>
2025-08-12 15:40:43 +0200

    simplify codegen_prepare

--- src/thorin/transform/codegen_prepare.cpp
+++ src/thorin/transform/codegen_prepare.cpp
@@ -9,26 +9,30 @@ struct CodegenPrepare : public Rewriter {
 
     DefMap<Continuation*> wrappers;
 
-    void make_wrapper(const Def* old_return_param) {
+    Continuation* make_wrapper(const Def* old_return_param) {
         assert(old_return_param);
-        assert(!wrappers.contains(old_return_param));
+        if (auto found = wrappers.lookup(old_return_param))
+            return *found;
         auto npi = instantiate(old_return_param->type())->as<FnType>();
         npi = dst().fn_type(npi->types());
         auto wrapper = dst().continuation(npi, old_return_param->debug());
         wrappers[old_return_param] = wrapper;
+        return wrapper;
     }
 
     const Def* rewrite(const Def* odef) override {
         if (auto app = odef->isa<App>()) {
             auto new_ops = Array<const Def*>(app->num_args(), [&](size_t i) -> const Def* {
-                if (auto wrapped = wrappers.lookup(app->arg(i)); wrapped.has_value()) {
+                auto op = app->arg(i);
+                if (op->isa<Param>() && op->type()->isa<ReturnType>()) {
                     // because wrappers bodies need the rewritten ret param, they need to be created lazily
                     // otherwise, rewriting the param would cause the whole scope to be rewritten first, before we get a chance to put anything in the map
-                    if (!(*wrapped)->has_body()) {
+                    auto wrapped = make_wrapper(op);
+                    if (!(wrapped)->has_body()) {
                         auto imported_param = instantiate(app->arg(i));
-                        (*wrapped)->jump(imported_param, (*wrapped)->params_as_defs(), imported_param->debug());
+                        wrapped->jump(imported_param, wrapped->params_as_defs(), imported_param->debug());
                     }
-                    return dst().return_point(*wrapped);
+                    return dst().return_point(wrapped);
                 } else {
                     return instantiate(app->arg(i));
                 }
@@ -40,20 +44,13 @@ struct CodegenPrepare : public Rewriter {
 };
 
 /// this pass makes sure the return param is only called directly, by eta-expanding any uses where it appears in another position
+// TODO: this effectively prevents tail-calls, this shouldn't run if the backend supports tail-calls
 void codegen_prepare(Thorin& thorin) {
     thorin.world().VLOG("start codegen_prepare");
     auto& src = thorin.world();
     auto destination = std::make_unique<World>(src);
     CodegenPrepare pass(src, *destination.get());
 
-    // step one: scan for top-level continuations and register their ret params
-    ScopesForest(src).for_each([&](Scope& scope) {
-        auto ret_param = scope.entry()->ret_param();
-        if (ret_param)
-            pass.make_wrapper(ret_param);
-    });
-
-    // step two: rewrite the world
     for (auto& external : src.externals())
         pass.instantiate(external.second);
 
